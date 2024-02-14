7f06bf74d56a6a9515f3f98a9ce54951a4e59aca
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-02-14 01:21:22 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/GataNina-Li/GataBot-MD.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/Wilsmac/FantasyBot-MD-v1.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
