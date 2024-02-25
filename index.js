858e35b60355ebfd77b98c5f22e9c0bcae913714
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-02-25 02:48:21 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Gata\nBot\nMD', {
+say('Fantasy\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
