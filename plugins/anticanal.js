fdef9d3c59487bf66695f1299820b812c9ea545d
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-02-29 11:59:44 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/anticanal.js
+++ plugins/anticanal.js
@@ -1,5 +1,6 @@
 /*
-let linkRegex = whatsapp.com/channel\/([0-9A-Za-z]{20,24})/i
+
+let linkRegex = /whatsapp.com/channel\/([0-9A-Za-z]{20,24})/i
 export async function before(m, { isAdmin, isBotAdmin, participants }) {
 if (m.isBaileys && m.fromMe)
 return !0
@@ -12,11 +13,11 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
-const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
-if (chat.antiLink && isGroupLink && !isAdmin) {
+const grupo = `https://whatsapp.com/channel`
+if (isAdmin && chat.antiCanal && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
+if (chat.antiCanal && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
-const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+const linkThisGroup = `https://whatsapp.com/channel/${await this.ChannelInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
 await conn.sendMessage(m.chat, {text: `${lenguajeGB['smsEnlaceWat']()} ${user}`, mentions: [m.sender]}, {quoted: m})
