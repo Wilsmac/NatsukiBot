095cc4942b369ad0aab401b80e6c03c30c2ff284
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-06 17:01:29 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/amensaje-en-espera.js
+++ plugins/amensaje-en-espera.js
@@ -22,13 +22,13 @@ const handler = async (m, { conn, usedPrefix }) => {
     if (filesDeleted === 0) {
       await conn.sendMessage(m.chat, {text: '*[✨] No se encontró ningún archivo que incluya la ID del chat*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*[✨] Se eliminaron ${filesDeleted} archivos de sesión*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*¸.☆¸.♡.¸ Se eliminaron ${filesDeleted} archivos de sesión*`}, {quoted: m});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
     await conn.sendMessage(m.chat, {text: '*[🌺] Ocurrió un error al eliminar los archivos de sesión*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*¡Hola! Ahora me ves?*\n\n*[♥️] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*⇴ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*¡Hola! ¿Ahora me ves?*\n\n*¸.☆¸.♡.¸ ~_Si el Bot no le responde a sus comandos por favor haga un pequeño spam_~*\n\n*⇴ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['fixmsgespera'];
 handler.tags = ['fix'];
