7f957d3386c1f74bb19ad0386a57155e1ec98a5c
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-05 23:06:59 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-allfake.js
+++ plugins/info-allfake.js
@@ -113,7 +113,7 @@ global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', s
 
 global.nombre = conn.getName(m.sender)
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
-global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: saludo + ` ${m.name}`, body: dev, sourceUrl: enlaces, thumbnailUrl: fotos }}}
+global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: saludo + ` ${wm}`, body: dev, sourceUrl: enlaces, thumbnailUrl: fotos }}}
 }
 
 export default handler
