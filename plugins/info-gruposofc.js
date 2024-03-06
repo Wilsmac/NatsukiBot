bb02b56138a5f819eb5f9677906abddd15ca64e1
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-06 00:20:21 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,5 +1,65 @@
 let media = './media/menus/Menuvid3.mp4'
 let handler = async (m, { conn, command }) => {
+const _0x44713f = _0x34b6;
+function _0x34b6(_0x355db8, _0x38a593) {
+    const _0x400972 = _0x5695();
+    return _0x34b6 = function (_0x16a5bb, _0x27e0c3) {
+        _0x16a5bb = _0x16a5bb - (0x1 * 0x12fa + -0x12dc + -0x16 * -0xb);
+        let _0x5e2a77 = _0x400972[_0x16a5bb];
+        return _0x5e2a77;
+    }, _0x34b6(_0x355db8, _0x38a593);
+}
+function _0x5695() {
+    const _0x4cecf4 = [
+        '10QkaAWA',
+        '50166towazJ',
+        'split',
+        '7930580BDnYXG',
+        '16rFbvUv',
+        ',;;;\x0aFN:',
+        ',\x0aitem1.TEL;waid=',
+        '1528180FrsxKo',
+        'sender',
+        '855510JLyYbE',
+        './media/menus/Menu1.jpg',
+        '2370321JHDFol',
+        '2784531yQdyIG',
+        'readFileSync',
+        '46058790kkljpF',
+        '25WXxsFt',
+        '\x0aitem1.X-ABLabell:Ponsel\x0aEND:VCARD'
+    ];
+    _0x5695 = function () {
+        return _0x4cecf4;
+    };
+    return _0x5695();
+}
+(function (_0x4d3b1a, _0x58b11c) {
+    const _0x423612 = _0x34b6, _0x34f90f = _0x4d3b1a();
+    while (!![]) {
+        try {
+            const _0x37ec1c = parseInt(_0x423612(0x111)) / (-0x1e * 0x11a + 0x2 * -0x4e4 + -0x891 * -0x5) * (-parseInt(_0x423612(0x112)) / (0x6b6 + 0x6a9 + -0xd5d)) + -parseInt(_0x423612(0x11d)) / (0x1ee + 0x255e + -0x2749) + -parseInt(_0x423612(0x118)) / (0x1 * 0x219d + -0x1156 + 0x17 * -0xb5) + parseInt(_0x423612(0x120)) / (-0x14a9 + -0x1 * -0xb5d + 0x3 * 0x31b) * (-parseInt(_0x423612(0x11a)) / (0x1e * 0x47 + 0x1588 + -0x1dd4)) + -parseInt(_0x423612(0x114)) / (-0x1c9c + 0x8a9 + 0x13fa) + parseInt(_0x423612(0x115)) / (0x12d * -0x1d + -0x257d + 0xb2 * 0x67) * (-parseInt(_0x423612(0x11c)) / (0x222 * 0x3 + -0x3af * -0x1 + -0x1 * 0xa0c)) + parseInt(_0x423612(0x11f)) / (0x89f * -0x1 + -0x15 * -0x81 + 0xf6 * -0x2);
+            if (_0x37ec1c === _0x58b11c)
+                break;
+            else
+                _0x34f90f['push'](_0x34f90f['shift']());
+        } catch (_0x47fdc1) {
+            _0x34f90f['push'](_0x34f90f['shift']());
+        }
+    }
+}(_0x5695, 0xf371d + -0x1 * -0x6f85a + -0xbed97));
+let fkontak2 = {
+    'key': { 'participant': '0@s.whatsapp.net' },
+    'message': {
+        'contactMessage': {
+            'displayName': wilmerofc,
+            'vcard': 'BEGIN:VCARD\x0aVERSION:3.0\x0aN:XL;' + author + _0x44713f(0x116) + author + _0x44713f(0x117) + m[_0x44713f(0x119)][_0x44713f(0x113)]('@')[-0x1484 * 0x1 + -0x666 + 0x1aea] + ':' + m['sender'][_0x44713f(0x113)]('@')[-0x162a + 0x546 + 0x10e4] + _0x44713f(0x110),
+            'jpegThumbnail': fs[_0x44713f(0x11e)](_0x44713f(0x11b)),
+            'thumbnail': fs['readFileSync']('./media/menus/Menu1.jpg'),
+            'sendEphemeral': !![]
+        }
+    }
+};
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `*BIENVENID@S ALOS GRUPOS OFICIAL DE FantasyBot-MD estos son los únicos grupos oficiales de fantasy, espero que te la pases muy bien.*
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
@@ -54,7 +114,7 @@ let str = `*BIENVENID@S ALOS GRUPOS OFICIAL DE FantasyBot-MD estos son los únic
 *${nnnttt6}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *hola :D*`
-await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
+await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak2)}
 /*conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n` + wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
