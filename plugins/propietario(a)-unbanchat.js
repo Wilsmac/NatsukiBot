da4767b32a046faeba5d315ca81997a7597529e2
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-06 13:20:47 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -5,6 +5,6 @@ global.db.data.chats[m.chat].isBanned = false
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
 handler.command = /^unbanchat$/i;
-handler.botAdmin = true
+//handler.botAdmin = true
 handler.admin = true
 export default handler;
