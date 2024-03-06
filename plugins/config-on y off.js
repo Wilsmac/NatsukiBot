f96ad1cb6f084a13715c8c855247c7eb42ccde5b
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-06 00:29:13 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -3,6 +3,66 @@ import fs from 'fs'
 
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner, text }) => { 
 //try{
+const _0x44713f = _0x34b6;
+function _0x34b6(_0x355db8, _0x38a593) {
+    const _0x400972 = _0x5695();
+    return _0x34b6 = function (_0x16a5bb, _0x27e0c3) {
+        _0x16a5bb = _0x16a5bb - (0x1 * 0x12fa + -0x12dc + -0x16 * -0xb);
+        let _0x5e2a77 = _0x400972[_0x16a5bb];
+        return _0x5e2a77;
+    }, _0x34b6(_0x355db8, _0x38a593);
+}
+function _0x5695() {
+    const _0x4cecf4 = [
+        '10QkaAWA',
+        '50166towazJ',
+        'split',
+        '7930580BDnYXG',
+        '16rFbvUv',
+        ',;;;\x0aFN:',
+        ',\x0aitem1.TEL;waid=',
+        '1528180FrsxKo',
+        'sender',
+        '855510JLyYbE',
+        './media/menus/Menu1.jpg',
+        '2370321JHDFol',
+        '2784531yQdyIG',
+        'readFileSync',
+        '46058790kkljpF',
+        '25WXxsFt',
+        '\x0aitem1.X-ABLabell:Ponsel\x0aEND:VCARD'
+    ];
+    _0x5695 = function () {
+        return _0x4cecf4;
+    };
+    return _0x5695();
+}
+(function (_0x4d3b1a, _0x58b11c) {
+    const _0x423612 = _0x34b6, _0x34f90f = _0x4d3b1a();
+    while (!![]) {
+        try {
+            const _0x37ec1c = parseInt(_0x423612(0x111)) / (-0x1e * 0x11a + 0x2 * -0x4e4 + -0x891 * -0x5) * (-parseInt(_0x423612(0x112)) / (0x6b6 + 0x6a9 + -0xd5d)) + -parseInt(_0x423612(0x11d)) / (0x1ee + 0x255e + -0x2749) + -parseInt(_0x423612(0x118)) / (0x1 * 0x219d + -0x1156 + 0x17 * -0xb5) + parseInt(_0x423612(0x120)) / (-0x14a9 + -0x1 * -0xb5d + 0x3 * 0x31b) * (-parseInt(_0x423612(0x11a)) / (0x1e * 0x47 + 0x1588 + -0x1dd4)) + -parseInt(_0x423612(0x114)) / (-0x1c9c + 0x8a9 + 0x13fa) + parseInt(_0x423612(0x115)) / (0x12d * -0x1d + -0x257d + 0xb2 * 0x67) * (-parseInt(_0x423612(0x11c)) / (0x222 * 0x3 + -0x3af * -0x1 + -0x1 * 0xa0c)) + parseInt(_0x423612(0x11f)) / (0x89f * -0x1 + -0x15 * -0x81 + 0xf6 * -0x2);
+            if (_0x37ec1c === _0x58b11c)
+                break;
+            else
+                _0x34f90f['push'](_0x34f90f['shift']());
+        } catch (_0x47fdc1) {
+            _0x34f90f['push'](_0x34f90f['shift']());
+        }
+    }
+}(_0x5695, 0xf371d + -0x1 * -0x6f85a + -0xbed97));
+let fkontak2 = {
+    'key': { 'participant': '0@s.whatsapp.net' },
+    'message': {
+        'contactMessage': {
+            'displayName': wilmerofc,
+            'vcard': 'BEGIN:VCARD\x0aVERSION:3.0\x0aN:XL;' + author + _0x44713f(0x116) + author + _0x44713f(0x117) + m[_0x44713f(0x119)][_0x44713f(0x113)]('@')[-0x1484 * 0x1 + -0x666 + 0x1aea] + ':' + m['sender'][_0x44713f(0x113)]('@')[-0x162a + 0x546 + 0x10e4] + _0x44713f(0x110),
+            'jpegThumbnail': fs[_0x44713f(0x11e)](_0x44713f(0x11b)),
+            'thumbnail': fs['readFileSync']('./media/menus/Menu1.jpg'),
+            'sendEphemeral': !![]
+        }
+    }
+};
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
@@ -597,14 +657,14 @@ ${m.isGroup ? `` : `${lenguajeGB.smsConfi9()}`}
 
 ╰▭ׄ▭ׅ▭ׅ▭ׄ▭ׅ▭ׄ  ✿ໍ ▭ׅ▭ׄ▭ׅ▭ׅ▭ׄ▭ׅ╯
 
-${wm}`, fkontak, { mentions: [aa,] })
+${wm}`, fkontak2, { mentions: [aa,] })
 //conn.sendMessage(m.chat, { text: texto }, { quoted: fkontak })
 //conn.sendMessage(m.chat, texto, {quoted: fkontak})        
 throw false
 }
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoRG']()}➥ *_${lenguajeGB['smsMens1']()}_* > ${type} 
 ➥ *_${lenguajeGB['smsMens2']()}_* > ${isEnable ? lenguajeGB.smsEncender() : lenguajeGB.smsApagar()} 
-➥ *_${lenguajeGB['smsMens3']()}_* > ${isAll ? lenguajeGB.smsMens4() : isUser ? '' : lenguajeGB.smsMens5()}\n*᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ*`, fkontak, m)}
+➥ *_${lenguajeGB['smsMens3']()}_* > ${isAll ? lenguajeGB.smsMens4() : isUser ? '' : lenguajeGB.smsMens5()}\n*᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ ֗ 𝆺𝅥 𝆭᭼۪۪۪۪۪ٜ♡۪۪۪۪۪۪۪۪۪ٜ᭼ִ*`, fkontak2, m)}
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
