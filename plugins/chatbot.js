d7fb6b2f1892f17e1a8e5c023bb90b01ea658947
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-06 18:35:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -3,127 +3,70 @@ let handler = m => m
 handler.all = async function (m) {
 const _0x5a72e7 = _0x1741;
 function _0x1741(_0xad93e9, _0x19e363) {
-    const _0x481e14 = _0x3f69();
-    return _0x1741 = function (_0x26de08, _0x520a49) {
-        _0x26de08 = _0x26de08 - (-0x56 * -0x3e + -0x13fa + 0x72);
-        let _0x228f84 = _0x481e14[_0x26de08];
-        return _0x228f84;
-    }, _0x1741(_0xad93e9, _0x19e363);
+const _0x1fd76b = _0xac65;
+function _0xac65(_0x80428a, _0x26a816) {
+    const _0xa498f4 = _0x2789();
+    return _0xac65 = function (_0x3c410f, _0x446f46) {
+        _0x3c410f = _0x3c410f - (-0x1b81 + 0x97b + 0x641 * 0x3);
+        let _0x40642f = _0xa498f4[_0x3c410f];
+        return _0x40642f;
+    }, _0xac65(_0x80428a, _0x26a816);
 }
-function _0x3f69() {
-    const _0xf187c2 = [
-        'split',
-        '3yvNBrR',
-        '23216230uAegsO',
-        '1098190ixDAzW',
-        '1842820whHglZ',
-        '\x0aitem1.X-ABLabell:Ponsel\x0aEND:VCARD',
-        ',;;;\x0aFN:',
-        '3309294YhGSHO',
-        './media/menus/Menu1.jpg',
-        'readFileSync',
-        '0@s.whatsapp.net',
-        '6996560aumDLt',
-        '4BgKqUI',
-        '2219080ZIMnRG',
-        'sender',
-        '9YEnwXH',
-        'BEGIN:VCARD\x0aVERSION:3.0\x0aN:XL;',
-        '9388974svkDGs'
-    ];
-    _0x3f69 = function () {
-        return _0xf187c2;
-    };
-    return _0x3f69();
-}
-(function (_0x4ebbf2, _0xf18a08) {
-    const _0x528b40 = _0x1741, _0x4a30e5 = _0x4ebbf2();
-    while (!![]) {
-        try {
-            const _0x1e7203 = -parseInt(_0x528b40(0x14c)) / (-0xf89 * -0x2 + -0x125 * 0xb + -0x5 * 0x3b2) * (parseInt(_0x528b40(0x14e)) / (0x968 + 0xfe * -0x1d + 0x1360)) + parseInt(_0x528b40(0x152)) / (0x173f + 0x6 * -0x2dd + 0xa * -0x9b) * (parseInt(_0x528b40(0x157)) / (0x3b0 + -0x4 * 0x313 + 0x8 * 0x114)) + -parseInt(_0x528b40(0x158)) / (-0x1eda + -0x94c + 0x5bd * 0x7) + -parseInt(_0x528b40(0x15c)) / (0xa3 * 0x33 + -0x25 * -0x76 + -0x13 * 0x29b) + parseInt(_0x528b40(0x14f)) / (-0x219a + 0xd62 + 0x47 * 0x49) + parseInt(_0x528b40(0x156)) / (0x11ed + 0x97a * -0x4 + 0x1403) + parseInt(_0x528b40(0x15a)) / (0x27 * 0x5 + 0x9 * -0xe4 + 0x6 * 0x137) * (parseInt(_0x528b40(0x14d)) / (-0x197d * -0x1 + -0x22e9 + 0x976));
-            if (_0x1e7203 === _0xf18a08)
-                break;
-            else
-                _0x4a30e5['push'](_0x4a30e5['shift']());
-        } catch (_0x34406a) {
-            _0x4a30e5['push'](_0x4a30e5['shift']());
-        }
-    }
-}(_0x3f69, -0x34 * -0x5aa3 + -0x12fd * 0xba + 0x1 * 0x93833));
-let wilmer = {
-    'key': { 'participant': _0x5a72e7(0x155) },
-    'message': {
-        'contactMessage': {
-            'displayName': fantasy,
-            'vcard': _0x5a72e7(0x15b) + author + _0x5a72e7(0x151) + author + ',\x0aitem1.TEL;waid=' + m[_0x5a72e7(0x159)][_0x5a72e7(0x15d)]('@')[0x1c25 + -0x99 * 0x12 + -0x1 * 0x1163] + ':' + m[_0x5a72e7(0x159)][_0x5a72e7(0x15d)]('@')[0x1e86 * -0x1 + -0x279 + 0x20ff * 0x1] + _0x5a72e7(0x150),
-            'jpegThumbnail': fs[_0x5a72e7(0x154)](_0x5a72e7(0x153)),
-            'thumbnail': fs[_0x5a72e7(0x154)](_0x5a72e7(0x153)),
-            'sendEphemeral': !![]
-        }
-    }
-};
-let wilmer2 = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wilmerofc, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wilmerofc},;;;\nFN:${wilmerofc},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}};
-let ftroli = { key: { participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: wilmerofc, status: 1, surface: 1, message: botdate, orderTitle: author, sellerJid: '0@s.whatsapp.net' }}}
-const _0x44713f = _0x34b6;
-function _0x34b6(_0x355db8, _0x38a593) {
-    const _0x400972 = _0x5695();
-    return _0x34b6 = function (_0x16a5bb, _0x27e0c3) {
-        _0x16a5bb = _0x16a5bb - (0x1 * 0x12fa + -0x12dc + -0x16 * -0xb);
-        let _0x5e2a77 = _0x400972[_0x16a5bb];
-        return _0x5e2a77;
-    }, _0x34b6(_0x355db8, _0x38a593);
-}
-function _0x5695() {
-    const _0x4cecf4 = [
-        '10QkaAWA',
-        '50166towazJ',
-        'split',
-        '7930580BDnYXG',
-        '16rFbvUv',
-        ',;;;\x0aFN:',
+function _0x2789() {
+    const _0x4a9b2d = [
         ',\x0aitem1.TEL;waid=',
-        '1528180FrsxKo',
+        'BEGIN:VCARD\x0aVERSION:3.0\x0aN:XL;',
+        'split',
+        '0@s.whatsapp.net',
+        '4107172PFAmOn',
+        '7146288mdxape',
+        '270284bcgVlD',
+        '9AEDIEB',
+        'chat',
         'sender',
-        '855510JLyYbE',
-        './media/menus/Menu1.jpg',
-        '2370321JHDFol',
-        '2784531yQdyIG',
-        'readFileSync',
-        '46058790kkljpF',
-        '25WXxsFt',
-        '\x0aitem1.X-ABLabell:Ponsel\x0aEND:VCARD'
+        '4419176GVlaYN',
+        '50uXmSck',
+        '1552131FGcSXH',
+        '\x0aitem1.X-ABLabell:Ponsel\x0aEND:VCARD',
+        '12434krlrnW',
+        '2483275NZXAgs'
     ];
-    _0x5695 = function () {
-        return _0x4cecf4;
+    _0x2789 = function () {
+        return _0x4a9b2d;
     };
-    return _0x5695();
+    return _0x2789();
 }
-(function (_0x4d3b1a, _0x58b11c) {
-    const _0x423612 = _0x34b6, _0x34f90f = _0x4d3b1a();
+(function (_0x54761c, _0x7ef734) {
+    const _0x2b21a3 = _0xac65, _0xb2c947 = _0x54761c();
     while (!![]) {
         try {
-            const _0x37ec1c = parseInt(_0x423612(0x111)) / (-0x1e * 0x11a + 0x2 * -0x4e4 + -0x891 * -0x5) * (-parseInt(_0x423612(0x112)) / (0x6b6 + 0x6a9 + -0xd5d)) + -parseInt(_0x423612(0x11d)) / (0x1ee + 0x255e + -0x2749) + -parseInt(_0x423612(0x118)) / (0x1 * 0x219d + -0x1156 + 0x17 * -0xb5) + parseInt(_0x423612(0x120)) / (-0x14a9 + -0x1 * -0xb5d + 0x3 * 0x31b) * (-parseInt(_0x423612(0x11a)) / (0x1e * 0x47 + 0x1588 + -0x1dd4)) + -parseInt(_0x423612(0x114)) / (-0x1c9c + 0x8a9 + 0x13fa) + parseInt(_0x423612(0x115)) / (0x12d * -0x1d + -0x257d + 0xb2 * 0x67) * (-parseInt(_0x423612(0x11c)) / (0x222 * 0x3 + -0x3af * -0x1 + -0x1 * 0xa0c)) + parseInt(_0x423612(0x11f)) / (0x89f * -0x1 + -0x15 * -0x81 + 0xf6 * -0x2);
-            if (_0x37ec1c === _0x58b11c)
+            const _0x4319d8 = -parseInt(_0x2b21a3(0xc2)) / (-0x67 * -0x2b + 0x4 * -0x14b + -0x61 * 0x20) * (-parseInt(_0x2b21a3(0xbf)) / (-0x766 * 0x5 + 0xbf6 * -0x3 + -0x26 * -0x1eb)) + parseInt(_0x2b21a3(0xc0)) / (0x37d * -0x2 + -0x139d + 0x1a9a) + parseInt(_0x2b21a3(0xc8)) / (-0x2 * -0xa97 + 0x2532 + -0xf9 * 0x3c) + -parseInt(_0x2b21a3(0xc3)) / (0x1d56 + -0x3f + -0x1d12 * 0x1) + -parseInt(_0x2b21a3(0xc9)) / (0x14aa + 0xdad * 0x2 + -0x2ffe) + -parseInt(_0x2b21a3(0xca)) / (0x162 * -0x8 + -0xaa9 + 0xae0 * 0x2) + -parseInt(_0x2b21a3(0xbe)) / (-0xb3 * -0x29 + -0x1267 + -0xa3c) * (-parseInt(_0x2b21a3(0xcb)) / (-0xce7 + -0x32d + 0x7d * 0x21));
+            if (_0x4319d8 === _0x7ef734)
                 break;
             else
-                _0x34f90f['push'](_0x34f90f['shift']());
-        } catch (_0x47fdc1) {
-            _0x34f90f['push'](_0x34f90f['shift']());
+                _0xb2c947['push'](_0xb2c947['shift']());
+        } catch (_0x31df51) {
+            _0xb2c947['push'](_0xb2c947['shift']());
         }
     }
-}(_0x5695, 0xf371d + -0x1 * -0x6f85a + -0xbed97));
-let fkontak2 = {
-    'key': { 'participant': '0@s.whatsapp.net' },
+}(_0x2789, 0x1 * 0xf9766 + 0xf67e7 * -0x1 + -0x2bb * -0x3bd));
+let wilmer = {
+    'key': {
+        'fromMe': ![],
+        'participant': _0x1fd76b(0xc7),
+        ...m[_0x1fd76b(0xcc)] ? { 'remoteJid': 'status@broadcast' } : {}
+    },
     'message': {
         'contactMessage': {
             'displayName': wilmerofc,
-            'vcard': 'BEGIN:VCARD\x0aVERSION:3.0\x0aN:XL;' + author + _0x44713f(0x116) + author + _0x44713f(0x117) + m[_0x44713f(0x119)][_0x44713f(0x113)]('@')[-0x1484 * 0x1 + -0x666 + 0x1aea] + ':' + m['sender'][_0x44713f(0x113)]('@')[-0x162a + 0x546 + 0x10e4] + _0x44713f(0x110),
-            'jpegThumbnail': fs[_0x44713f(0x11e)](_0x44713f(0x11b)),
-            'thumbnail': fs['readFileSync']('./media/menus/Menu1.jpg'),
+            'vcard': _0x1fd76b(0xc5) + wilmerofc + ',;;;\x0aFN:' + wilmerofc + _0x1fd76b(0xc4) + m[_0x1fd76b(0xbd)]['split']('@')[-0x13 * -0xe5 + 0xdaf + -0x1eae] + ':' + m[_0x1fd76b(0xbd)][_0x1fd76b(0xc6)]('@')[0x19a + 0x7 * -0x53f + 0x231f] + _0x1fd76b(0xc1),
+            'jpegThumbnail': imagen1,
+            'thumbnail': imagen1,
             'sendEphemeral': !![]
         }
     }
 };
+let ftroli = { key: { participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: wilmerofc, status: 1, surface: 1, message: botdate, orderTitle: author, sellerJid: '0@s.whatsapp.net' }}}
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let chat = global.db.data.chats[m.chat]
 let name = conn.getName(m.sender)
@@ -139,7 +82,7 @@ conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 
 
 if (/^Hola|Holi|hola$/i.test(m.text) ) { //sin prefijo
-  conn.reply(m.chat, `*Holi, como estás?* 🥺♥️`, wilmer2, m) //wm, null, [['Menu', '#menu']], m) botones :V
+  conn.reply(m.chat, `*Holi, como estás?* 🥺♥️`, wilmer, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
 }
 
