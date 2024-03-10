10b7ed2948378b1b05ff101e6a3ee85e70cef809
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-10 00:01:52 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/prueba.js
@@ -0,0 +1,12 @@
+import { createHash } from 'crypto'
+let handler = async function (m, { args, command }) {
+
+if (command == 'nose1' || command == 'nose') {
+conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', 'Hola', 'status@broadcast')
+}
+
+}
+handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <numero de serie>')
+handler.tags = ['nose']
+handler.command = /^nose|nose1?$/i
+export default handler
