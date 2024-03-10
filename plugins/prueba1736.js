54bc1c71686dec1e281155a0495955fff9ee493c
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-10 00:14:07 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/prueba1736.js
@@ -0,0 +1,22 @@
+import { createHash } from 'crypto'
+let handler = async function (m, { args, command }) {
+
+if (command == 'now' || command == 'no1') {
+if (!args[0]) throw `Hola amigo :D`
+let user = global.db.data.users[m.sender]
+var sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
+if (args[0] !== sn) throw `Hola amigo`
+user.registered = false
+m.reply(`*Hola amigo :D.*`)
+}
+
+if (command == 'sn' || command == 'wilmer') {
+conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', 'Te amo', 'status@broadcast')
+}
+
+}
+handler.help = ['', 'ister'].map(v => 'wilmer' + v + ' Que haces?')
+handler.tags = ['rg']
+handler.command = /^unreg(ister)|wilmer|ns?$/i
+handler.register = true
+export default handler
