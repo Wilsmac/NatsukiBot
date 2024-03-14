d560be19cf886501727962f9193175b7be6dffc5
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-14 11:31:49 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/jadibot-deleteSesion.js
+++ plugins/jadibot-deleteSesion.js
@@ -1,21 +1,29 @@
-
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
 import path, { join } from 'path'
+
 let handler  = async (m, { conn, usedPrefix, command}, args) => {
 let parentw = conn
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let uniqid = `${who.split`@`[0]}`
 let userS = `${conn.getName(who)}`
-if (global.conn.user.jid !== conn.user.jid) return conn.sendMessage(m.chat, {text: lenguajeGB.smsJBDel() + `\n\n*https://api.whatsapp.com/send/?phone=${global.conn.user.jid.split`@`[0]}&text=${usedPrefix + command}&type=phone_number&app_absent=0*`}, { quoted: m }) 
-else {
-await conn.sendMessage(m.chat, { text: lenguajeGB.smsJBAdios() }, { quoted: m })}
+
+if (global.conn.user.jid !== conn.user.jid) {
+return conn.sendMessage(m.chat, {text: lenguajeGB.smsJBDel() + `\n\n*https://api.whatsapp.com/send/?phone=${global.conn.user.jid.split`@`[0]}&text=${usedPrefix + command}&type=phone_number&app_absent=0*`}, { quoted: m }) 
+} else {
 try {
-fs.rmdir("./FantasyJadiBot/" + uniqid, { recursive: true, force: true })
+await fs.rmdir("./FantasyJadiBot/" + uniqid, { recursive: true, force: true })
+await conn.sendMessage(m.chat, { text: lenguajeGB.smsJBAdios() }, { quoted: m })
 await conn.sendMessage(m.chat, { text : lenguajeGB.smsJBCerrarS() } , { quoted: m })
 } catch(err) {
-console.error(userS + ' ' + lenguajeGB.smsJBErr(), err)   
-}}
-handler.command = /^(deletesesion|eliminarsesion|borrarsesion|delsesion|cerrarsesion)$/i
+if (err.code === 'ENOENT' && err.path === `./FantasyJadiBot/${uniqid}`) {
+await conn.sendMessage(m.chat, { text: "Usted no es Sub Bot" }, { quoted: m })
+} else {
+console.error(userS + ' ' + lenguajeGB.smsJBErr(), err)
+}}}
+}
+
+handler.command = /^(deletesess?ion|eliminarsesion|borrarsesion|delsess?ion|cerrarsesion)$/i
 handler.private = true
 handler.fail = null
+
 export default handler
