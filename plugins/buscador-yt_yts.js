034158f6f32360381ef78ebb505ae47104ea6b74
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-19 00:26:32 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/buscador-yt_yts.js
+++ plugins/buscador-yt_yts.js
@@ -20,11 +20,11 @@ let textoInfo = `${lenguajeGB['smsAvisoIIG']()}${mid.smsYtlist(usedPrefix)}
 let teks = results.all.map((v, i) => {
 let link = v.url;
 vids_.urls.push(link);
-return `[${i + 1}]\n❤️꙰༻ *${mid.smsYT1}:*  ${v.title}
-⁖🩵꙰༻ *${mid.smsYT4}:* ${v.url}
-⁖💜꙰༻ *${mid.smsYT5}:* ${v.timestamp}
-⁖💚꙰༻ *${mid.smsYT9}:* ${v.ago}
-⁖🧡꙰༻ *${mid.smsYT10}:* ${v.views}`}).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+return `[${i + 1}]\n✨⃞⃝⃟ *${mid.smsYT1}:*  ${v.title}
+✨⃞⃝⃟ *${mid.smsYT4}:* ${v.url}
+✨⃞⃝⃟ *${mid.smsYT5}:* ${v.timestamp}
+✨⃞⃝⃟ *${mid.smsYT9}:* ${v.ago}
+✨⃞⃝⃟ *${mid.smsYT10}:* ${v.views}`}).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
 conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, fkontak, m)
 global.videoList.push(vids_);
 } catch {    
@@ -41,7 +41,7 @@ export default handler
 /*import yts from "yt-search"
 let handler = async (m, { text, conn, args, command, usedPrefix }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
+if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀`, fkontak,  m)
 try {
 let search = await yts(args.join(" "))
 let listAudio = []
