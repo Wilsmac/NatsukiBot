167acd29133b7255db2e4208a42be279c2f4011b
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-19 01:32:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -4,6 +4,7 @@ import yts from 'yt-search'
 import ytdl from 'ytdl-core'
 import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": 'ғᴀɴᴛᴀsʏʙᴏᴛ-MD', 'jpegThumbnail': null}}}
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7}\n*${usedPrefix + command} https://youtu.be/c5gJRzCi0f0*`, fkontak, m)
 let youtubeLink = '';
@@ -24,7 +25,7 @@ throw `${lenguajeGB['smsAvisoMG']()}${mid.smsYT2(usedPrefix, command)} ${usedPre
 }} else {
 throw `${lenguajeGB['smsAvisoMG']()}${mid.smsYT2(usedPrefix, command)}${usedPrefix}playlist <texto>*`;
 }}}  
-await conn.reply(m.chat, lenguajeGB['smsAvisoEG']() + mid.smsVid, fkontak, m)
+await conn.reply(m.chat, lenguajeGB['smsAvisoEG']() + mid.smsVid, fakemek, m)
 try {
 let qu = args[1] || '360'
 let q = qu + 'p'
