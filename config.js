ae0f95a99339211e4ad966efbdcba5503571943d
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-18 18:59:48 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- config.js
+++ config.js
@@ -238,6 +238,12 @@ global.accountsgb = [channel1, channel2, channel3, tk, ig, yt, paypal, fb, ths, 
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18]
 global.gataVidMenu = ['./media/menus/Menuvid1.mp4', './media/menus/Menuvid2.mp4', './media/menus/Menuvid3.mp4']
 global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13]
+
+
+//----------------------------------------------------
+
+global.fantasyImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13]
+
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
