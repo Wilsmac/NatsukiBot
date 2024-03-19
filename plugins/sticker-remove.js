d5605bd822a211b26303f658ea4a6f8d80d9b3bc
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-18 18:55:07 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/sticker-remove.js
+++ plugins/sticker-remove.js
@@ -36,46 +36,7 @@ for (let i = 0; i < mensajes.length; i++) {
 await new Promise(resolve => setTimeout(resolve, 1000))
 await conn.sendMessage(m.chat, { text: mensajes[i], edit: key })
 }}
+/*
 
-
-
-
-
-/*import uploadImage from '../lib/uploadImage.js'
-import { sticker } from '../lib/sticker.js'
-
-let handler = async (m, { conn, text, args }) => {
-let stiker = false
-let json, key
-
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (/image/g.test(mime) && !/webp/g.test(mime)) {
-let buffer = await q.download()
-let media = await (uploadImage)(buffer)
-json = await (await fetch(`https://aemt.me/removebg?url=${media}`)).json()
-stiker = await sticker(false, json.url.result, global.packname, global.author)
-} else if (text) {
-json = await (await fetch(`https://aemt.me/removebg?url=${text.trim()}`)).json()
-} else return m.reply(`* `(jpg, jpeg o png)` *`)
-
-await mensajesEditados(conn, m, key)
-await conn.sendMessage(m.chat, { text: waitttttt, edit: key })
-await conn.sendMessage(m.chat, { image: { url: json.url.result }, caption: null }, { quoted: m })
-await conn.sendFile(m.chat, stiker ? stiker : await sticker(false, json.url.result, global.packname, global.author), 'sticker.webp', '', null, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wilmerofc, body: '• STICKER •', mediaType: 2, sourceUrl: canalofc, thumbnail: gataImg.getRandom()}}})
-}
-handler.command = /^(s?removebg|quitarfondo)$/i
-export default handler
-
-const isUrl = (text) => {
-const urlRegex = /^(https?):\/\/[^\s/$.?#]+\.(jpe?g|png)$/i
-return urlRegex.test(text)
-}
-
-async function mensajesEditados(conn, m, key) {
-const mensajes = [ waitt, waittt, waitttt, waittttt ]
-key = await conn.sendMessage(m.chat, { text: wait, quoted: m })
-for (let i = 0; i < mensajes.length; i++) {
-await new Promise(resolve => setTimeout(resolve, 1000))
-await conn.sendMessage(m.chat, { text: mensajes[i], edit: key })
-}}*/
+              𝐖𝐢𝐥𝐦𝐞𝐫 𝐨𝐟𝐜 | 𝐈 𝐥𝐨𝐯𝐞 𝐲𝐨𝐮 𝐦𝐲 𝐥𝐨𝐯𝐞 𝐀𝐛𝐢🌹
+*/
