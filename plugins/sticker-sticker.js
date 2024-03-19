c70f1c9c613ec51fb1525d9d42a6434bd55d809f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-19 15:45:22 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -22,7 +22,7 @@ if (new Date - user.lastmiming < 10000) return await conn.reply(m.chat, `*ESPERA
       } catch (e) {
         console.error(e)
       } finally {
-await conn.reply(m.chat, `${eg} ${await}`, m)
+await conn.reply(m.chat, `${eg} hola`, m)
         if (!stiker) {
           if (/webp/g.test(mime)) out = await webp2png(img)
           else if (/image/g.test(mime)) out = await uploadImage(img)
