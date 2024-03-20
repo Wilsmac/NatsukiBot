7b4497a241802efcfaee4ec2efd95d858ea39f6c
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-20 16:17:58 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/p-prueba2.js
@@ -0,0 +1,17 @@
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+try {  
+if (global.conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+conn.reply(m.chat, stdout.toString(), m)}
+//} catch {
+var update = execSync('git remote set-url origin https://github.com/Wilsmac/FantasyBot-MD-v1.git && git pull')
+await m.reply(`hola :D\nupdate.toString()`)
+} catch {
+await m.reply(`${fg}`) 
+}}
+handler.help = ['update9']
+handler.tags = ['owner']
+handler.command = /^update9|actualizar9$/i
+handler.rowner = true
+export default handler 
