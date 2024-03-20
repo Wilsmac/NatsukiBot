7b0e11533097071a8e70604782acf737712c168a
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-20 15:43:08 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -2,12 +2,12 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m});
+    const { key } = await conn.sendMessage(m.chat, {text: `\`[10%]\``}, {quoted: m});
 await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key});
+await conn.sendMessage(m.chat, {text: `\`[60%]\``, edit: key});
 await delay(1000 * 1);
-await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key});
-await conn.sendMessage(m.chat, {text: `𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩`, edit: key});
+await conn.sendMessage(m.chat, {text: `\`[100%]\``, edit: key});
+await conn.sendMessage(m.chat, {text: `𝐑𝐄𝐈𝐍𝐈𝐂𝐈𝐀𝐍𝐃𝐎`, edit: key});
     process.send('reset')
   } else throw 'eh'
 }
