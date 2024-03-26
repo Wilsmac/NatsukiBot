dc67d927fd213cb29b260592663cf6f9aa656acf
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-26 17:46:46 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/sticker-:v.js
+++ plugins/sticker-:v.js
@@ -1,4 +1,4 @@
-*import { sticker } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
 let handler = async(m, { conn }) => {
 //if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 *#on stickers* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍`
 if (!db.data.chats[m.chat].stickers && m.isGroup) throw 0
