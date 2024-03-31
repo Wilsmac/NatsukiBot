699237dfc239a62e44d6a058a692d5e08d073343
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-31 10:17:03 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/p-prueba2.js
+++ plugins/p-prueba2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 try {  
 if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, stdout.toString(), m)}
+conn.reply(m.chat, 'actualizaciones realizadas, cambios realizados' + stdout.toString(), m)}
 //} catch {
 var update = execSync('git remote set-url origin https://github.com/Wilsmac/FantasyBot-MD-v1.git && git pull')
 await m.reply('actualizaciones realizadas, cambios realizados' + stdout.toString())
