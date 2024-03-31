980e9e21a8ba6eed14bbc6d513f059cc5b7903f3
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-31 01:14:51 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/nosé.js
@@ -0,0 +1,46 @@
+const { DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser, PHONENUMBER_MCC } = await import('@whiskeysockets/baileys')
+import Pino from 'pino'
+import fs from 'fs'
+import syntaxerror from 'syntax-error'
+import { makeWASocket, protoType, serialize } from './lib/simple.js'
+
+
+
+let phoneNumber = global.botNumberCode
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+const methodCode = !!phoneNumber || process.argv.includes("code")
+const MethodMobile = process.argv.includes("mobile")
+browser: ['Ubuntu', 'Edge', '110.0.1587.56']
+
+
+global.conn = makeWASocket
+if (!fs.existsSync(`./${authFile}/creds.json`)) {
+if (command == '.serbot77' || command == 'serbot77') { 
+if (!conn.authState.creds.registered) {
+let addNumber
+if (!!phoneNumber) {
+addNumber = phoneNumber.replace(/[^0-9]/g, '')
+} else {
+do {
+phoneNumber = await m.reply(`Por favor, Ingrese el número de WhatsApp.`)
+phoneNumber = phoneNumber.replace(/\D/g,'')
+} while (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v)))
+rl.close()
+addNumber = phoneNumber.replace(/\D/g, '')
+
+setTimeout(async () => {
+let codeBot = await conn.requestPairingCode(addNumber)
+codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
+m.reply(`CÓDIGO DE VINCULACIÓN`)
+m.reply(`${codeBot}`)
+}, 2000)
+}}}
+}
+
+conn.isInit = false
+conn.well = false
+
+if (!opts['test']) {
+if (global.db) setInterval(async () => {
+if (global.db.data) await global.db.write()
+if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp', "FantasyJadiBot"], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '2', '-type', 'f', '-delete'])))}, 30 * 1000)}
