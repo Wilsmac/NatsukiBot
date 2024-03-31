6f84086055a9f892465c581779c81b279ed78e59
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-03-31 09:51:28 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/propietario(a)-reporte.js
+++ plugins/propietario(a)-reporte.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `${mg}*𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙚𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚*\n\n*𝙀𝙅𝙀𝙈𝙋𝙇𝙊:*\n*${usedPrefix + command} el comando ${usedPrefix}infobot no funciona.*`
-if (text.length < 8) throw `${fg} ✨ *Mínimo 10 caracteres para hacer El Reporte.*`
-if (text.length > 1000) throw `${fg} 😼 *Máximo 1000 caracteres para hacer El Reporte.*`
-let teks = `*╭━━[ 𝙍𝙀𝙋𝙊𝙍𝙏𝙀 ]━━━⬣*\n*┃*\n*┃* *𝙉𝙐𝙈𝙀𝙍𝙊*\n┃ ✦ Wa.me/${m.sender.split`@`[0]}\n*┃*\n*┃* *𝙈𝙀𝙉𝙎𝘼𝙅𝙀*\n*┃* ✦ ${text}\n*┃*\n*╰━━━━━━━━━━━━━━━━━━⬣*`
-//conn.reply('19393844141@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
+if (!text) throw `𝙻𝙾 𝙰𝙷 𝚄𝚂𝙰𝙳𝙾 𝙳𝙴 𝙼𝙰𝙽𝙴𝚁𝙰 𝙸𝙽𝙲𝙾𝚁𝙴𝙲𝚃𝙰\n\n*𝙴𝚂𝙲𝚁𝙸𝙱𝙰 𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝙴*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no funka.*`
+if (text.length < 8) throw `*Mínimo 10 caracteres para hacer El Reporte.*`
+if (text.length > 1000) throw `*Máximo 1000 caracteres para hacer El Reporte.*`
+let teks = `╭━━[ 𝚁𝙴𝙿𝙾𝚁𝚃𝙴 ]━━━\n*┃*\n*┃* *𝙽𝚄𝙼𝙴𝚁𝙾*\n┃ ✦ Wa.me/${m.sender.split`@`[0]}\n*┃*\n*┃* *𝙼𝙴𝙽𝚂𝙰𝙹𝙴*\n*┃* ✦ ${text}\n*┃*\n╰━━━━━━━━━━━━━━━━━━`
+//conn.reply('50101139@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
 //contextInfo: {
 //mentionedJid: [m.sender]
 //}})
@@ -11,7 +11,7 @@ conn.reply('50250101139@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks,
 contextInfo: {
 mentionedJid: [m.sender]
 }})
-  m.reply(`╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊* ⊱💚⊱╮\n\n*El reporte ha sido enviado a mí Creador(a) Tendrá una respuesta pronto. De ser Falso será Ignorado el reporte.*`)
+  m.reply(`𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝚁 𝙰𝙷 𝚂𝙸𝙳𝙾 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 𝙼𝙸 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾(𝙰) 𝙰 𝙳𝙴 𝚂𝙴𝚁 𝙵𝙰𝙻𝚂𝙾 𝚂𝙾𝙻𝙾 𝚂𝙴𝚁𝙰 𝙸𝙶𝙽𝙾𝚁𝙰𝙳𝙾 𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝙴.`)
 
 }
 
