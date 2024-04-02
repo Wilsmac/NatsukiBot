4dde07a96b74c19c2b7ad7e83b843e0eaf117ae7
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-02 15:02:06 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/rol-rpg_pareja_mipareja.js
+++ plugins/rol-rpg_pareja_mipareja.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix }) => { // @${global.db.data.users[m.sender].pasangan.split('@')[0]}
+/*let handler = async (m, { conn, usedPrefix }) => { // @${global.db.data.users[m.sender].pasangan.split('@')[0]}
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let usuario = conn.getName(who)
@@ -16,3 +16,4 @@ global.db.data.users[m.sender].pasangan = ""
 handler.command = /^(sinceridad|mipareja|miamor|minovio|minovia|mylove)$/i
 handler.group = true
 export default handler
+*/
