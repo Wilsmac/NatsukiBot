bfc47182f464b05f18bd31a9df0f827e3f5f0617
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-05 14:46:24 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/rpg-rob.js
+++ plugins/rpg-rob.js
@@ -32,4 +32,3 @@ hours = (hours < 10) ? "0" + hours : hours
 minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
 return hours + " Hora(s) " + minutes + " Minuto(s)"}
-
