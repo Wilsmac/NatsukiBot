fabbeada6639440c097d096083f4beacc5ae8d8f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-08 01:55:48 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-comands.js
+++ plugins/info-comands.js
@@ -12,6 +12,7 @@ const openaiii = new OpenAIApi(configuration)
 let handler = async (m, { conn, command, usedPrefix, args, text }) => {
 
 const isCommand1 = /^(gemini|bard)$/i.test(command)
+const isCommand2 = /^(ai)$/i.test(command)
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 async function reportError(e) {
@@ -33,7 +34,18 @@ await m.reply(res.result)
 reportError(e)
 }
 break
+case isCommand2:
+if (!text) throw `\`Escriba un texto usando el comando para usar a chatgpt :D\``
+await conn.sendPresenceUpdate('composing', m.chat)       
+try {
+var apii = await fetch(`https://aemt.me/openai?text=${text}`)
+var res = await apii.json()
+await m.reply(res.result)
+} catch (e) {
+reportError(e)
+}
+break
 }}
-handler.command = /^(gemini|bard)$/i
+handler.command = /^(ai|gemini|bard)$/i
 //handler.register = true
 export default handler
