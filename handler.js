dbf959c969e1de386a3ce24c37eaa677adfbb1b5
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-13 18:06:48 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- handler.js
+++ handler.js
@@ -1381,8 +1381,16 @@ if (opts['autoread']) await this.readMessages([m.key])
 if (settingsREAD.autoread2) await this.readMessages([m.key])  
 //if (settingsREAD.autoread2 == 'true') await this.readMessages([m.key])    
 	    
-if (db.data.chats[m.chat].reaction && m.text.match(/(ción|dad|aje|oso|izar|mente|pero|tion|age|ous|ate|and|but|ify)/gi)) {
+if (db.data.chats[m.chat].reaction && m.text.match(/(dad|aje|izar|mente|pero|but)/gi)) {
 let emot = pickRandom(["✨", "🥹", "🤧", "🫠", "🌺", "🤨", "🫠", "🥲", "🫥", "😶‍🌫️", "😹", "🙀", "❤️‍🩹", "❤️‍🔥", "😶"])
+if (!m.fromMe) return this.sendMessage(m.chat, { react: { text: emot, key: m.key }})
+}
+if (db.data.chats[m.chat].reaction && m.text.match(/(enable)/gi)) {
+let emot = ["☑️"]
+if (!m.fromMe) return this.sendMessage(m.chat, { react: { text: emot, key: m.key }})
+}
+if (db.data.chats[m.chat].reaction && m.text.match(/(disable)/gi)) {
+let emot = ["✖️"]
 if (!m.fromMe) return this.sendMessage(m.chat, { react: { text: emot, key: m.key }})
 }
 function pickRandom(list) { return list[Math.floor(Math.random() * list.length)]}
