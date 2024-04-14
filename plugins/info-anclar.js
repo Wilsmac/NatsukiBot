f93522a3d5660184806654b9bfd38aed3d26445f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-13 22:01:47 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-anclar.js
+++ plugins/info-anclar.js
@@ -1,1 +1,20 @@
-//uwu
+//códigos desarrollado por Wilmer oficial
+// https://github.com/Wilsmac
+
+                                  let handler = function (m) {
+                 if (!m.quoted) throw false
+
+             let { chat, fromMe, isBaileys } = m.quoted
+    
+     if (!fromMe) throw false
+
+            if (!isBaileys) throw '> INFORMACION\nESE MENSAJE NO FUE ENVIADO POR MI Y POR LO TANTO NO PUEDO ELIMINARLO.'
+
+         conn.sendMessage(chat, { delete: m.quoted.vM.key })
+}
+handler.help = ['del', 'delete']
+handler.tags = ['tools']
+handler.command = /^del(ete)?$/i
+handler.group = true
+handler.admin = true
+export default handler
