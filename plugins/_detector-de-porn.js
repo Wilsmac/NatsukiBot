8261b383b7fb8c1166e299131dc1013cd3ae90c7
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-14 23:04:36 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_detector-de-porn.js
+++ plugins/_detector-de-porn.js
@@ -4,3 +4,14 @@ import fetch from 'node-fetch'
 let { downloadContentFromMessage } = (await import(global.baileys))
 import path from 'path'  
 let handler = m => m
+handler.before = async function (m, { conn, __dirname }) {
+let media, link, buffer
+try{
+let q = m
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (!(/sticker|image/.test(mime)) || m.mtype == 'viewOnceMessageV2') return
+let IsTel = /^image\/(png|jpe?g)$/.test(mime)
+if (IsTel) {
+media = await q.download()
+link = await uploadImage(media)
+}
