2e36fa670193f27583630649b197cf09fb74c997
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-14 23:02:38 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_detector-de-porn.js
+++ plugins/_detector-de-porn.js
@@ -3,3 +3,4 @@ import { webp2png } from '../lib/webp2mp4.js'
 import fetch from 'node-fetch'
 let { downloadContentFromMessage } = (await import(global.baileys))
 import path from 'path'  
+let handler = m => m
