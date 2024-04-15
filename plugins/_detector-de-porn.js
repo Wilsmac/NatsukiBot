beaef3bd811ccb2f495ac200be9977ba0a9834f5
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-14 23:00:18 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/_detector-de-porn.js
@@ -0,0 +1,5 @@
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+import fetch from 'node-fetch'
+let { downloadContentFromMessage } = (await import(global.baileys))
+import path from 'path'  
