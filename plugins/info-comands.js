c956de0ba5364d42d16cd95446628cfeab79d8e8
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-15 19:18:23 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-comands.js
+++ plugins/info-comands.js
@@ -1,18 +1,23 @@
 import translate from '@vitalets/google-translate-api'
 import googleIt from 'google-it'
 import fetch from 'node-fetch'
+import { spawn } from 'child_process'
 import gpt from 'api-dylux'
 import gtts from 'node-gtts'
+import { execSync } from 'child_process'
 import {readFileSync, unlinkSync} from 'fs'
 import fs from 'fs' 
 import {Configuration, OpenAIApi} from 'openai';
 const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key})
 const openaiii = new OpenAIApi(configuration)
 
-let handler = async (m, { conn, command, usedPrefix, args, text }) => {
+let handler = async (m, { conn, command, usedPrefix, args, text, isOwner, isROwner, participants, groupMetadata  }) => {
+let fkontak, who, user, number, bot, bant
+fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 const isCommand1 = /^(gemini|bard)$/i.test(command)
 const isCommand2 = /^(ai)$/i.test(command)
+const isCommand3 = /^(join|nuevogrupo|newgrupo|unete)$/i.test(command)
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 async function reportError(e) {
@@ -45,7 +50,21 @@ await m.reply(res.result)
 reportError(e)
 }
 break
+case isCommand3:
+user = m.sender.split('@')[0] 
+let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
+let [_1, code1] = link.match(linkRegex) || []
+if (!code1) return m.reply(lenguajeCD.smsJoin1(usedPrefix, command))
+try {      
+if ( isOwner || m.fromMe) {
+await m.reply(lenguajeCD.smsJoin2())
+let res1 = await conn.groupAcceptInvite(code1)
+await conn.sendMessage(res1, { text: lenguajeCD.smsJoin(user), mentions: (await conn.groupMetadata(`${res1}`)).participants.map(v => v.id) }, [user], { quoted: fkontak })
+}} catch (e) {
+reportError(e)
+}        
+break
 }}
-handler.command = /^(ai|gemini|bard)$/i
+handler.command = /^(ai|gemini|bard|join|nuevogrupo|newgrupo|unete)$/i
 //handler.register = true
 export default handler
