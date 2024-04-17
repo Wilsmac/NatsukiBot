a3be136b0f035c890798e30812ffbebd6ef009c4
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 12:04:23 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/vtraba-global.js
+++ plugins/vtraba-global.js
@@ -4,7 +4,7 @@
 
               let scan = await parent.sendMessage(m.chat, `Hola` m)
   setTimeout(() => {
-    parent.sendMessage(m.chat, { delete: scan.key })
+    parent.sendMessage(m.chat, { delete: scan })
   }, 50000) //50 segundos
 
 handler.help = ['holi'];
