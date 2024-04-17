f978abff29d4e19caebb21fb03c202237ecc5f67
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-16 19:21:20 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_detector-de-porn.js
+++ plugins/_detector-de-porn.js
@@ -1,15 +1,24 @@
+let { downloadContentFromMessage } = (await import(global.baileys))
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 import fetch from 'node-fetch'
-let { downloadContentFromMessage } = (await import(global.baileys))
-import path from 'path'  
+import axios from 'axios'
+import path from 'path'
+global.enlace = null
+
 let handler = m => m
-handler.before = async function (m, { conn, __dirname }) {
-let media, link, buffer
+handler.before = async function (m, { conn, __dirname, isBotAdmin }) {
+let chat = global.db.data.chats[m.chat]
+let media, link, buffer = false
+let web = /https?:\/\/\S+/
+if (!isBotAdmin || chat.delete || !m.isGroup) return
+if (!chat.antiPorn) return 
 try{
 let q = m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (!(/sticker|image/.test(mime)) || m.mtype == 'viewOnceMessageV2') return
+let delet = q.key.participant
+let bang = q.key.id
+if (/sticker|image/.test(mime) || m.mtype == 'viewOnceMessageV2') {
 let IsTel = /^image\/(png|jpe?g)$/.test(mime)
 if (IsTel) {
 media = await q.download()
@@ -22,17 +31,45 @@ if (type == 'imageMessage') {
 media = await downloadContentFromMessage(msg[type], 'image')
 buffer = Buffer.from([])
 for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])}
+buffer = Buffer.concat([buffer, chunk])
+}
 link = await uploadImage(buffer)
 }}
-if (link) {
+if (m.mtype == 'stickerMessage') {
+try {
+link = await webp2png(await q.download())
+} catch {
+link = false
+}}
+} else {
+if (q.text || web.test(q.text)) {
+await IsEnlace(q.text).then(result => {
+link = result ? enlace : false
+}).catch(error => {
+link = false
+})
+} else return  
+}
+if (!link) return 
 const response = await fetch(`https://api.alyachan.dev/api/porn-detector?image=${link}&apikey=wilmermacu`)
 const result = await response.json()
-await m.reply(link)
-if (result.status && result.data && result.data.isPorn) {
-}
+enlace = null
+if (result.status && result.data.isPorn) {
+await m.reply('*La imagen contiene imágenes +18 para adultos!!*')
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 }} catch (error) {
-await m.reply(error.toString())
-}
-}                
+console.log(error)
+}}                
 export default handler
+async function IsEnlace(texto) {
+const regexEnlace = /https?:\/\/\S+/
+const match = texto.match(regexEnlace)
+if (match) {
+enlace = match[0]
+const response = await fetch(enlace, { method: 'HEAD' })
+const contentType = response.headers.get('content-type')
+if (contentType && (contentType.startsWith('image/jpeg') || contentType.startsWith('image/jpg') || contentType.startsWith('image/png') || contentType.startsWith('image/webp'))) {
+return true
+}}
+return false
+}
