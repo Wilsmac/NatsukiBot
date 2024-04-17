820e4a1d8f3c14087748667b5b05429e88c43274
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 12:02:37 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/vtraba-global.js
+++ plugins/vtraba-global.js
@@ -1,1 +1,12 @@
 // fue sacado por algún mal uso del comando :v
+
+
+
+              let scan = await parent.sendMessage(m.chat, `Hola` m)
+  setTimeout(() => {
+    parent.sendMessage(m.chat, { delete: scan.key })
+  }, 50000) //50 segundos
+
+handler.help = ['holi'];
+handler.command = /^(holi)$/i;
+export default handler;
