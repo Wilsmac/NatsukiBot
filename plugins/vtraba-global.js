ba7f4b161b2331fa79969d2645f4ec5b81b935d3
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 12:09:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/vtraba-global.js
+++ plugins/vtraba-global.js
@@ -1,12 +1,12 @@
 // fue sacado por algún mal uso del comando :v
 
-
+ let handler  = async (m, { conn }) => {
 
               
   setTimeout(() => {
     parent.sendMessage(m.chat, { delete: `Hola` })
   }, 50000) //50 segundos
-
+}
 handler.help = ['holi'];
 handler.command = /^(holi)$/i;
 export default handler;
