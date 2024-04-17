b99a76206ae4004f80419d0116d8d6f2f277eef5
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 12:26:30 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/vtraba-global.js
+++ plugins/vtraba-global.js
@@ -6,7 +6,7 @@
    let scan = await conn.sendFile(m.chat, await `https://telegra.ph/file/6114942024c7658478830.jpg`, m)
   setTimeout(() => {
     conn.sendMessage(m.chat, { delete: scan.key })
-  }, 50000) //50
+  }, 3000) //50
 }
 handler.help = ['holi'];
 handler.command = /^(holi)$/i;
