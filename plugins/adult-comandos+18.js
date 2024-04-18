cbb4c99d3403b2bd231bf9eed4790558da8c006b
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 22:32:38 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -51,13 +51,19 @@ const handler = async (m, {command, conn}) => {
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
+  if (command == 'panties') {
+    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/panties.json`)).data;
+    const url = await res[Math.floor(res.length * Math.random())];
+    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
+  }
+
   if (command == 'yuri') {
     const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/yuri.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 };
-handler.help = ['nsfwloli', 'yuri', 'tetas', 'booty', 'ecchi', 'porno', 'randomxxx', 'pechos'];
-handler.command = ['nsfwloli', 'yuri', 'tetas', 'booty', 'ecchi', 'porno', 'randomxxx', 'pechos'];
+handler.help = ['nsfwloli', 'yuri', 'tetas', 'booty', 'ecchi', 'porno', 'randomxxx', 'pechos', 'panties'];
+handler.command = ['nsfwloli', 'yuri', 'tetas', 'booty', 'ecchi', 'porno', 'randomxxx', 'pechos', 'panties'];
 handler.tags = ['nsfw'];
 export default handler;
