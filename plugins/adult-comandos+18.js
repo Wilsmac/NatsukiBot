eaeffe014f4d8b7d9808953e9268bcb4301c5ccd
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 23:28:44 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -48,9 +48,11 @@ const handler = async (m, {command, conn}) => {
   if (command == 'pechos') {
     const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/pechos.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
+   let pechos = conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
-
+  setTimeout(() => {
+    conn.sendMessage(m.chat, { delete: pechos })
+  }, 30000) //50
   if (command == 'panties') {
     const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/panties.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
