77151a5d94856c57c19cdde8be5a5e80c9158dfb
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-17 20:57:45 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,3 +1,33 @@
+const xpperlimit = 350;
+const handler = async (m, {conn, command, args}) => {
+  const datas = global
+
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count;
+    global.db.data.users[m.sender].limit += count;
+    conn.reply(m.chat, `
+┌─「 *NOTA DE PAGO* 」
+‣ *Compra nominal* : + ${count}💎 
+‣ *Gastado* : -${xpperlimit * count} XP
+└──────────────`, m);
+  } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m);
+};
+handler.help = ['Buy', 'Buyall'];
+handler.tags = ['xp'];
+handler.command = ['buy', 'buyall'];
+
+handler.disabled = false;
+
+export default handler;
+
+
+
+
+
+
 /*const xpperlimit = 300
 let handler = async (m, { conn, command, args, usedPrefix }) => {
   let count = command.replace(/^buyy/i, '')
