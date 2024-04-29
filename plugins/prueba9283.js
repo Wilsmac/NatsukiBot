49dcd1ce681f0469fa16172d985febb8715301b6
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-29 13:16:50 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/prueba9283.js
+++ plugins/prueba9283.js
@@ -1,4 +1,4 @@
-import { getDevice } from '@whiskeysockets/baileys'
+/*import { getDevice } from '@whiskeysockets/baileys'
 import fs from 'fs'
 import moment from 'moment-timezone'
 import fetch from 'node-fetch'
@@ -162,4 +162,4 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}  
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')} */
