12b41d119ec992b4b710549efb5ea35a6dec2de4
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-30 19:27:56 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/message.js
+++ plugins/message.js
@@ -11,13 +11,24 @@ let msg = generateWAMessageFromContent(m.chat, {
       "deviceListMetadata": {},
       "deviceListMetadataVersion": 2
         },
-        interactiveMessage:  proto.Message.InteractiveMessage.Header.create({
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: "test"
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "test"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
             title: "test",
             subtitle: "test",
             hasMediaAttachment: false
           }),
           nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-   buttons: [
+            buttons: [
+              {
+                "name": "single_select",
+                "buttonParamsJson": "{\"title\":\"title\",\"sections\":[{\"title\":\"title\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+              },
               {
                 "name": "quick_reply",
                 "buttonParamsJson": "{\"display_text\":\".estado\",\"id\":\"message\"}"
@@ -25,6 +36,14 @@ let msg = generateWAMessageFromContent(m.chat, {
               {
                  "name": "cta_url",
                  "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com\",\"merchant_url\":\"https://www.google.com\"}"
+              },
+              {
+                 "name": "cta_call",
+                 "buttonParamsJson": "{\"display_text\":\"call\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "cta_copy",
+                 "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
               }
        ],
    })
