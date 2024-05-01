b7f9267b1dff58f9775899ce592154c0095e4fb2
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-01 00:00:14 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/muslos.js
@@ -0,0 +1,65 @@
+// códigos totalmente hecho por Wilmer oficial 
+// https://github.com/Wilsmac
+// deje créditos si usará el código ☄️
+
+
+import fetch from 'node-fetch'
+      const handler = async (m, {conn, command, usedPrefix}) => {
+            if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ 𝐈𝐍𝐅𝐎 ] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙰𝙲𝚃𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'
+             switch (command) {
+                  case 'muslos'
+                          const url = await muslos[Math.floor(Math.random() * pack.length)]
+           conn.sendMessage(m.chat, {image: {url: url}, caption: `_Muslos 🥵_`}, {quoted: m})
+         break
+        }
+   }
+
+handler.command = /^(muslos)$/i
+export default handler
+
+
+global.muslos = [
+"https://i.imgur.com/uGzFdVD.jpg",
+"https://i.imgur.com/XLNHZDB.jpg",
+"https://i.imgur.com/97wc7tf.jpg",
+"https://i.imgur.com/xUS74sl.jpg",
+"https://i.imgur.com/T3J0a0V.jpg",
+"https://i.imgur.com/ZXGstbh.jpg",
+"https://i.imgur.com/tFnHUzu.jpg",
+"https://i.imgur.com/bLuKAkv.jpg",
+"https://i.imgur.com/KqwqwWT.jpg",
+"https://i.imgur.com/HNRFA2d.jpg",
+"https://i.imgur.com/maj2vCy.jpg",
+"https://i.imgur.com/Wtka9lQ.jpg",
+"https://i.imgur.com/iPxVsRN.jpg",
+"https://i.imgur.com/CGxMkwR.jpg",
+"https://i.imgur.com/Vcv90HQ.jpg",
+"https://i.imgur.com/N0kcxJh.jpg",
+"https://i.imgur.com/opFmmKu.jpg",
+"https://i.imgur.com/lVe9ay0.jpg",
+"https://i.imgur.com/nfTWjuE.jpg",
+"https://i.imgur.com/ojCMG7G.jpg",
+"https://i.imgur.com/KoV3pwz.jpg",
+"https://i.imgur.com/M8MnbCE.jpg",
+"https://i.imgur.com/RVGS8Bc.jpg",
+"https://i.imgur.com/kV063RZ.jpg",
+"https://i.imgur.com/xYQZ73z.jpg",
+"https://i.imgur.com/TzG2ozI.jpg",
+"https://i.imgur.com/d957bf9.jpg",
+"https://i.imgur.com/Sz7O8Na.jpg",
+"https://i.imgur.com/ots1C4k.jpg",
+"https://i.imgur.com/6LoLi5T.jpg",
+"https://i.imgur.com/vMdbESU.jpg",
+"https://i.imgur.com/0wTQqWo.jpg",
+"https://i.imgur.com/h5Uzg3B.jpg",
+"https://i.imgur.com/hD2ZDaL.jpg",
+"https://i.imgur.com/xQdMTML.jpg",
+"https://i.imgur.com/0l5nTKw.jpg",
+"https://i.imgur.com/55SqTTm.jpg",
+"https://i.imgur.com/SIqr2Ju.jpg",
+"https://i.imgur.com/YxAw706.jpg",        
+"https://i.imgur.com/rssXY6l.jpg",
+"https://i.imgur.com/sh9KrZP.jpg",        
+"https://i.imgur.com/RJFX4Md.jpg",
+"https://i.imgur.com/eDZ1qIC.jpg"        
+]
