3fe4782c80f9c9b3bc1b532c0cd5470e7591a56f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-30 21:43:41 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/random-meme.js
+++ plugins/random-meme.js
@@ -1,51 +1,11 @@
 /*   Creditos - ig : https://www.instagram.com/fg98._/    */
 
-import pkg from '@whiskeysockets/baileys';
-const { generateWAMessageFromContent, proto } = pkg
-
 import hispamemes from 'hispamemes'
 let handler = async (m, {command, conn}) => {
-const url = await hispamemes.meme()  
-let msg = generateWAMessageFromContent(m.chat, {
-  viewOnceMessage: {
-    message: {
-      "messageContextInfo": {
-      "deviceListMetadata": {},
-      "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: ""
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "uwu"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: "Hola",
-            subtitle: "prueba",
-            hasMediaAttachment: false
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".estado\",\"id\":\"message\"}"
-              },
-              {
-                 "name": "cta_url",
-                 "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://github.com/Wilsmac/FantasyBot-MD-v1\",\"merchant_url\":\"https://www.google.com\"}"
-              }
-       ],
-   })
-    })
-    }
-  }
-}, {})
+const url = await hispamemes.meme()
 
-
-//await conn.relayMessage(url, msg.key.remoteJid, msg.message, { messageId: msg.key.id })
-//conn.sendFile(m.chat, url, 'error.jpg', msg, m)}
-await conn.relayMessage(m.chat, `_${msg}_`.trim(), author, url, msg, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `🤣🤣🤣`, m)}
+//await conn.relayMessage(m.chat, `_${msg}_`.trim(), author, url, msg, m)}
 handler.help = ['meme']
 handler.tags = ['random']
 handler.command = /^(meme|memes)$/i
