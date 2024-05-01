76c723cee96e114e981102436b23fe91d8b0f110
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-01 00:03:47 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/muslos.js
+++ plugins/muslos.js
@@ -4,11 +4,11 @@
 
 
 import fetch from 'node-fetch'
-const handler = async (m, {conn, command, usedPrefix}) => {
- if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ 𝐈𝐍𝐅𝐎 ] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙰𝙲𝚃𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'
-  switch (command) {
-    case 'muslos'
-    const url = await muslos[Math.floor(Math.random() * pack.length)]
+          const handler = async (m, {conn, command, usedPrefix}) => {
+               if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ 𝐈𝐍𝐅𝐎 ] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙰𝙲𝚃𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'
+        switch (command) {
+  case 'muslos'
+                const url = await muslos[Math.floor(Math.random() * muslos.length)]
 conn.sendMessage(m.chat, {image: {url: url}, caption: `_Muslos 🥵_`}, {quoted: m})
          break
         }
