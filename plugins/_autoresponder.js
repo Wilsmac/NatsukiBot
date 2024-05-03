a39b89970dbf158afb3485fcaeb05d7a0ef48560
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-02 23:54:43 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelWL.id, newsletterName: channelWL.name, serverMessageId: -1 }, forwardingScore: 200, externalAdReply: { title: packname, body: author, thumbnailUrl: fantasyMenu.getRandom(), sourceUrl: canalofc, mediaType: 1, renderLargerThumbnail: false }
+contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelWL.id, newsletterName: channelWL.name, serverMessageId: -1 }, forwardingScore: 200, externalAdReply: { title: packname, body: author, thumbnailUrl: fantasyMenu.getRandom(), sourceUrl: canalofc, mediaType: 1, renderLargerThumbnail: false }})}
 
 //contextInfo: { externalAdReply: { title: '𝑭𝒂𝒏𝒕𝒂𝒔𝒚𝑩𝒐𝒕-𝑴𝑫', body: '𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄', sourceUrl: `https://github.com/Wilsmac/FantasyBot-MD-v1`, thumbnail: imagen2}}})}
     
