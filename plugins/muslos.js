99a26048f099b304ca2f2587f02a5f0f2edcbf85
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-02 22:46:05 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/muslos.js
+++ plugins/muslos.js
@@ -9,10 +9,11 @@ let = texto `☄️`
         if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ 𝐈𝐍𝐅𝐎 ] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙰𝙲𝚃𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
          switch (command) {
     case 'muslos':
+
                const url = await muslos[Math.floor(Math.random() * muslos.length)];
 
 conn.sendButton(m.chat, texto, `Muslos 🥵\n` + wm, url, [
-['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🥵', '.muslos'], fkontak)}
+['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🥵', '.muslos']], fkontak)}
 
      //conn.sendMessage(m.chat, {image: {url: url}, caption: `_Muslos 🥵_`}, {quoted: m});
        break;
