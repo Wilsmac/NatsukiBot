a86c86bf68f790d7d375dbcd579d3d1e2d28e9c6
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-02 20:38:07 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -486,8 +486,8 @@ throw false
 global.opts['swonly'] = isEnable
 break
 default:
-if (!/[01]/.test(command)) return conn.sendMessage(m.chat, listMessage, {quoted: fkontak})
-/*if (!/[01]/.test(command)) return await conn.reply(m.chat, `${lenguajeGB.smsConfi10()}\n\n🌟 ${lenguajeGB.smsConfi2()} *@${toUser}*
+/*if (!/[01]/.test(command)) return conn.sendMessage(m.chat, listMessage, {quoted: fkontak})*/
+if (!/[01]/.test(command)) return await conn.reply(m.chat, `${lenguajeGB.smsConfi10()}\n\n🌟 ${lenguajeGB.smsConfi2()} *@${toUser}*
 
 ${lenguajeGB.smsConfi3()}
 ${lenguajeGB.smsConfi4()}
@@ -669,7 +669,7 @@ ${m.isGroup ? `` : `${lenguajeGB.smsConfi9()}`}
 ╰▭ׄ▭ׅ▭ׅ▭ׄ▭ׅ▭ׄ  ✿ໍ ▭ׅ▭ׄ▭ׅ▭ׅ▭ׄ▭ׅ╯
 
 ${wm}`, fkontak2, { mentions: [aa,] })
-//conn.sendMessage(m.chat, { text: texto }, { quoted: fkontak })*/
+//conn.sendMessage(m.chat, { text: texto }, { quoted: fkontak })
 //conn.sendMessage(m.chat, texto, {quoted: fkontak})        
 throw false
 }
