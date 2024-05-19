dc9bb12d4ec939cbfe1ddb3b9fea8f9ac306a1c7
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-19 16:50:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/anticanal.js
@@ -0,0 +1,28 @@
+import yts from 'yt-search'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
+  let res = await yts(text)
+  let vid = res.videos[0]
+  if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+  let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+        //const url = 'https://www.youtube.com/watch?v=' + videoId
+        m.react('🎧') 
+  let play = `
+        jdjs*
+┌──────────────
+▢  *título*: ${vid.title}
+▢  *uwu* ${vid.ago}
+▢  uwu* ${vid.timestamp}
+▢  *uwu ${vid.views.toLocaleString()}
+└──────────────`
+ await conn.sendButton(m.chat, play, wm, thumbnail, [
+    ['🎶 MP3', `${usedPrefix}menu`],
+    ['🎥 MP4', `${usedPrefix}menu`]
+  ], null, [['Canal', `${canalofc}`]], m)
+}
+handler.help = ['play']
+handler.tags = ['dl']
+handler.command = ['play', 'playvid']
+handler.disabled = false
+
+export default handler
