ce8fa931771cc9b0581ea530a64c807afe3572df
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-23 08:40:54 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/marker-lolice.js
+++ plugins/marker-lolice.js
@@ -1,7 +1,7 @@
-const handler = async (m, {conn}) => {
+const handler = async (m, {conn: cd}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+ cd.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
+    avatar: await cd.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'error.png', '*LLAMEN A LA POLICÍA!! 😱*', m);
 };
 handler.help = ['lolice'];
