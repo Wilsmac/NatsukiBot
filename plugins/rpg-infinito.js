57b82b59ecabecb2a0c137e26d493d109fc470c2
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-23 08:36:44 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/rpg-infinito.js
+++ plugins/rpg-infinito.js
@@ -1,9 +1,9 @@
 //let { MessageType } = require('@adiwajshing/baileys')
 let { MessageType } = (await import(global.baileys)).default
 //SOLO USA SI ERES EL/LA PROPIETARIO(A) DEL BOT PARA TENER TODO ILIMITADO O USA EL COMANDO PREMIUM Jajaj
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn: natsuki }) => {
     let user = global.db.data.users[m.sender]
-        conn.reply(m.chat, `*ÉXITO!!*`, m)
+        natsuki.reply(m.chat, `*ÉXITO!!*`, m)
         global.db.data.users[m.sender].money = Infinity
         global.db.data.users[m.sender].limit = Infinity
         global.db.data.users[m.sender].level = Infinity
