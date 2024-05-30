d8809fdb1cce5933354fa365918b04097338dc3e
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-29 19:34:43 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/crategc.js
+++ plugins/crategc.js
@@ -1,24 +1,24 @@
-/*let handler = async (m, { conn: natsuki, text }) => {
+let handler = async (m, { conn: natsuki, text }) => {
 
 if (!text) 
-  return natsuki.reply('𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁, 𝙴𝚂𝙲𝚁𝚄𝙱𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝚁 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 シ︎')
+  return m.reply('𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁, 𝙴𝚂𝙲𝚁𝚄𝙱𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝚁 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 シ︎')
 try{
            m.react('⚙️')
-mnatsuki.reply('ღ 𝚁𝙴𝙰𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝚇𝙸𝚃𝙾𝚂𝙰𝙼𝙴𝙽𝚃𝙴\n\n𝙲𝚁𝙴𝙰𝙽𝙳𝙾 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾...')
+m.reply('ღ 𝚁𝙴𝙰𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝚇𝙸𝚃𝙾𝚂𝙰𝙼𝙴𝙽𝚃𝙴\n\n𝙲𝚁𝙴𝙰𝙽𝙳𝙾 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾...')
 
 let natsuki2 = await conn.groupCreate(text, [m.sender])
 let l = await conn.groupInviteCode(group.gid)
 
 let invite = 'https://chat.whatsapp.com/' + l;
 
-natsuki.reply('*l:* ' + invite)
+m.reply('*l:* ' + invite)
 } catch (e) {
 
-natsuki.reply(`𝙷𝚄𝙱𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝙸𝙽𝙴𝚂𝙿𝙴𝚁𝙰𝙳𝙾`)
+m.reply(`𝙷𝚄𝙱𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝙸𝙽𝙴𝚂𝙿𝙴𝚁𝙰𝙳𝙾`)
    }
       }
 handler.help = ['creargc *<nombre>*']
 handler.tags = ['owner']
-handler.command = /^(creargrupo|creargroup)$/
+handler.command = /^(creargc|creargrupo|creargroup)$/
 handler.owner = true
-export default handler*/
+export default handler
