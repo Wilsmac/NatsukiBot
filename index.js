7948350d38f3688439e3290e2c45e08ced539988
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-31 20:02:37 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Fantasy\nBot\nMD', {
+say('Natsuki\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
