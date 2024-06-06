ab36a9ab5cb6ce2976780caf4cb5dab4fb697e9f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-05 21:10:41 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-comands2.js
+++ plugins/info-comands2.js
@@ -1,6 +1,6 @@
 
 
-/*let handler = async (m, { conn, command, usedPrefix, args, text, isOwner, isROwner, participants, groupMetadata  }) => {
+let handler = async (m, { conn, command, usedPrefix, args, text, isOwner, isROwner, participants, groupMetadata  }) => {
 let fkontak, who, user, number, bot, bant
 fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
@@ -47,5 +47,5 @@ reportError(e)
 break
 }}
 handler.command = /^(contactgc|privacionline)$/i
-//handler.register = true
-export default handler*/
+handler.owner = true
+export default handler
