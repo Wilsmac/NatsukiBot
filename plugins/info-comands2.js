0196ad842f603e04dc96c03c05144e2ed01666c9
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-05 21:50:01 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-comands2.js
+++ plugins/info-comands2.js
@@ -1,45 +1,23 @@
 let handler = async (m, {conn}) => {
+  const isCommand1 = /^(contactgc)$/i.test(command)
 
-const isCommand1 = /^(contactgc)$/i.test(command)
-const isCommand2 = /^(privacionline)$/i.test(command)
-
-async function reportError(e) {
-await m.reply(lenguajeCD['smsMalError3']() + '\n*' + lenguajeCD.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeCD.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeCD.smsMensError2()} ` + usedPrefix + command)
-console.log(`❗❗ ${lenguajeCD['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}
-
-switch (true) {
-
-case isCommand1:
+    async function reportError(e) {
+     await m.reply(lenguajeCD['smsMalError3']() + '\n*' + lenguajeCD.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeCD.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeCD.smsMensError2()} ` + usedPrefix + command)
+   console.log(`❗❗ ${lenguajeCD['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+ console.log(e)
+    }
+  switch (true) {
+ case isCommand1:
 let id = text ? text : m.chat  
-const GD = 'all' // 'contacts'
+const GD = 'contacts'
 await conn.updateGroupsAddPrivacy(GD)
 m.reply(`AGREGANDO LA CONFIGURACIÓN...`)
 try {  
 } catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
-}}
-} catch (e) {
-reportError(e)
-}
-break
-
-case isCommand2:
-const CD = 'match_last_seen'
-await conn.updateOnlinePrivacy(CD)
-m.reply(`AGREGANDO LA CONFIGURACIÓN...`)
-try {  
-} catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
-}}
-} catch (e) {
-reportError(e)
+reportError(e) 
 }
 break
 }}
-handler.command = /^(contactgc|privacionline)$/i
-handler.rowner = true
+handler.command = /^(contactgc)$/i
+handler.owner = true
 export default handler
