9c06a8d6c8b865e45059dbc2363a68f1ed8021c6
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-05 21:09:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-comands2.js
+++ plugins/info-comands2.js
@@ -4,7 +4,8 @@
 let fkontak, who, user, number, bot, bant
 fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-const isCommand1 = /^(gemini|bard)$/i.test(command)
+const isCommand1 = /^(contactgc)$/i.test(command)
+const isCommand2 = /^(privacionline)$/i.test(command)
 
 async function reportError(e) {
 await m.reply(lenguajeCD['smsMalError3']() + '\n*' + lenguajeCD.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeCD.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeCD.smsMensError2()} ` + usedPrefix + command)
@@ -14,18 +15,37 @@ console.log(e)
 
 switch (true) {
 
+
+
 case isCommand1:
-if (!text) throw `ingrese un texto usando el comando para usar a Gemini`
-await conn.sendPresenceUpdate('composing', m.chat)       
-try {
-var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
-var res = await apii.json()
-await m.reply(res.result)
+let id = text ? text : m.chat  
+const GD = 'all' // 'contacts'
+await conn.updateGroupsAddPrivacy(GD)
+m.reply(`AGREGANDO LA CONFIGURACIÓN...`)
+try {  
+} catch (e) {
+await m.reply(`${fg}`) 
+return console.log(e)
+}}
+} catch (e) {
+reportError(e)
+}
+break
+
+case isCommand2:
+const CD = 'match_last_seen'
+await conn.updateOnlinePrivacy(CD)
+m.reply(`AGREGANDO LA CONFIGURACIÓN...`)
+try {  
+} catch (e) {
+await m.reply(`${fg}`) 
+return console.log(e)
+}}
 } catch (e) {
 reportError(e)
 }
 break
 }}
-handler.command = /^(ai|gemini|bard)$/i
+handler.command = /^(contactgc|privacionline)$/i
 //handler.register = true
 export default handler*/
