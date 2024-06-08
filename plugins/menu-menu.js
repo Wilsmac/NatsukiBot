79882a13332f1b22386a299bbb64f1dc09090615
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-08 17:43:05 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -705,7 +705,6 @@ console.log(e)}}
 
 //handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|menucompleto|allmenu|allm|m|\?)$/i
 handler.command = /^(menucompleto|menú|menú|Menú|allmenu|\?)$/i
-//handler.register = true
 export default handler
 
 const more = String.fromCharCode(8206)
