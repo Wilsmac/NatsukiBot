f33b141e7c10555532530989368343bce0ecb71b
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-08 14:50:42 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -84,7 +84,7 @@ if (!/web|desktop|unknown/gi.test(dispositivo)) {
 let menu = `
 \`Fecha y hora:\` undefined 
    ↷ ɪɴғᴏ ᴅᴇ ʙᴏᴛ
-${(conn.user.jid == global.conn.user.jid ? '' : `\n*𝙉𝙖𝙩𝙨𝙪𝙠𝙞 [𝙎𝙪𝙗 𝘽𝙤𝙩] @${global.conn.user.jid.split`@`[0]}*`) || ''}
+${(conn.user.jid == global.conn.user.jid ? '' : `\n*𝙉𝙖𝙩𝙨𝙪𝙠𝙞 [𝙎𝙪𝙗 𝘽𝙤𝙩] https://wa.me/${global.conn.user.jid.split`@`[0]}*`) || ''}
  *${lenguajeCD['smsTotalUsers']()}* ➺ _${Object.keys(global.db.data.users).length}_
 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🍁❈┉━━━━─
  *${lenguajeCD['smsVersion']()}* ➺ _${vs}_
