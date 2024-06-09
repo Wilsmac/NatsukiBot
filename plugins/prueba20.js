38dedb0670c6071ec7fea8617f6653f986d27aa4
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-09 10:00:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/prueba20.js
+++ plugins/prueba20.js
@@ -5,18 +5,18 @@ const messages = [
 'Descripción 1',
 'Footer 1',
 'https://telegra.ph/file/b95059bd21a2bbbd0218b.jpg',
-[['Botón 1', 'id1'], ['Botón 2', 'id2']],
+[['Botón 1', '.menu'], ['Botón 2', '.menu']],
 'Texto para copiar 1',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
 [[
 'Lista 1', 'Sección 1',
- 'Titulo 1', usedPrefix + 'menu', 'Descripcion 1', 'id1',
+ 'Titulo 1', usedPrefix + 'menu', 'Descripcion 1', '.menu',
 ]]],
 [
 'Descripción 2',
 'Footer 2',
 'https://telegra.ph/file/d57ee60a1674082040ad0.jpg',
-[['Botón 1', 'id1'], ['Botón 2', 'id2']],
+[['Botón 1', '.menu'], ['Botón 2', '.menu']],
 'Texto para copiar 2',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
 [[
