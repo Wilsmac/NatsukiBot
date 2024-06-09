b8f24533f5a256ef420498284f01a63270265142
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-09 08:41:35 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/prueba20.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+
+const messages = [
+['Descripción 1', 'Footer 1', 'https://telegra.ph/file/b31cd03f716d362b33716.jpg', [], '', [], []],
+['Descripción 2', 'Footer 2', 'https://telegra.ph/file/98c672926fbc35a4b9948.jpg', [], '', [], []],
+['Descripción 3', 'Footer 3', 'https://telegra.ph/file/f689a972cfc1c5efff189.jpg', [], '', [], []],
+]
+
+
+
+
+await conn.sendCarousel(m.chat, 'Texto', 'Linea', 'TEXTO', messages, m)
+
+}
+
+handler.command = /^(lph)$/i
+export default handler
