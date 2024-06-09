747359688338797e47f02747b7e3f75a6cfb0afb
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-09 14:55:09 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/prueba20.js
+++ plugins/prueba20.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-const messages = [
+/*const messages = [
 [
 'Descripción 1', 
 'Footer 1',
@@ -17,6 +17,36 @@ const messages = [
 'Descripción 2',
 'Footer 2',
 'https://telegra.ph/file/d57ee60a1674082040ad0.jpg',
+[['Botón 1', 'id1'], ['Botón 2', 'id2']],
+'Texto para copiar 2',
+[['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
+[[
+'Lista', 
+'Sección ',  
+'Titulo',  
+'Tema',  
+'Descripcion', 
+'Id',
+]]]]*/
+
+
+const messages = [
+[
+null, null,
+'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+[['Buscar con Google', 'id1'], ['Botón 2', 'id2']],
+null,
+[['Enlace', 'https://example.com/link1'], [null, null]],
+[
+            [[['Lista'], [ 'Sección'], ['Título]', ['[Tema'], ['Descripción', 'Id']]]],
+            [['Sección 1', 'Título 1', 'Tema 1', 'Descripción 1', 'Id 1']],
+            [['Sección 2', 'Título 2', 'Tema 2', 'Descripción 2', 'Id 2']]
+        ]
+    ],
+[
+'Descripción 2',
+'Footer 2',
+'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
 [['Botón 1', 'id1'], ['Botón 2', 'id2']],
 'Texto para copiar 2',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
