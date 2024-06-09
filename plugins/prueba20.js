45bf75bc0ce8b85211c61aa9c16fdd3ce45c603d
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-09 13:05:12 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/prueba20.js
+++ plugins/prueba20.js
@@ -2,35 +2,33 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 
 const messages = [
 [
-'Descripción 1',
+'Descripción 1', 
 'Footer 1',
-'https://telegra.ph/file/b95059bd21a2bbbd0218b.jpg',
-[['Botón 1', '.menu'], ['Botón 2', '.menu']],
+'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+[['Botón 1', 'id1'], ['Botón 2', 'id2']],
 'Texto para copiar 1',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
-[[
-'Lista 1', 'Sección 1',
- 'Titulo 1', usedPrefix + 'menu', 'Descripcion 1', '.menu',
-]]],
+[
+            [[['Lista'], [ 'Sección'], ['Título]', ['[Tema'], ['Descripción', 'Id']]]],
+            [['Sección 1', 'Título 1', 'Tema 1', 'Descripción 1', 'Id 1']],
+            [['Sección 2', 'Título 2', 'Tema 2', 'Descripción 2', 'Id 2']]
+        ]
+    ],
 [
 'Descripción 2',
 'Footer 2',
-'https://telegra.ph/file/d57ee60a1674082040ad0.jpg',
-[['Botón 1', '.menu'], ['Botón 2', '.menu']],
+'https://telegra.ph/file/b31cd03f716d362b33716.jpg',
+[['Botón 1', 'id1'], ['Botón 2', 'id2']],
 'Texto para copiar 2',
 [['Enlace 1', 'https://example.com/link1'], ['Enlace 2', 'https://example.com/link2']],
 [[
 'Lista', 
-   'Sección ',  
-          'id',  
-            'Titulo',  
-               'Subtitulo', 
-                    'Descripcion',
-             ]
-     ]
-          ]
-              ]
-
+'Sección ',  
+'Titulo',  
+'Tema',  
+'Descripcion', 
+'Id',
+]]]]
 
 await conn.sendNatsukisel(m.chat, 'Texto', 'Linea', 'TEXTO', messages, m)
 
