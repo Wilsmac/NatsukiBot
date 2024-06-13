ff929c1f9ae0822c0044903f535e94ce7c975fbf
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-12 20:30:20 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/pruebas.js
+++ plugins/pruebas.js
@@ -15,6 +15,45 @@ const {
                             itemCount : 999,
                             status: 1,
                             surface : 1,
+                            message: `HELLO WORLD :3\n5 Menit`, 
+                            orderTitle: `WILMER OFICIAL\n5 Menit`,
+                            thumbnail: await (await fetch(Menu1)).buffer(),
+                            sellerJid: '0@s.whatsapp.net' 
+                          }
+                        }
+                      }
+          //m.reply(teks)
+          let jawab = `*Bot aktif selama*\n5 Menit\n\n_©ᴋɪɴɢ ᴏғ ʙᴇᴀʀ_`.trim()
+conn.sendMessage(m.chat,{text:wm}, {quoted: ftrol})
+}
+
+
+handler.help = ['troli']
+
+handler.tags = ['ᴘᴇɴᴅɪɴɢ sᴛᴜғғ']
+
+handler.command = /^troli$/i
+export default handler
+
+
+
+/*import fs from 'fs'
+import fetch from 'node-fetch'
+ let handler  = async (m, { conn, usedPrefix: _p }) => {
+const {
+    MessageType,
+    Mimetype
+} = (await import("@whiskeysockets/baileys")).default;
+
+           const ftrol = {
+  key : {
+                          participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    orderMessage: {
+                            itemCount : 999,
+                            status: 1,
+                            surface : 1,
                             message: `okto\n5 Menit`, // Wilmer :3
                             orderTitle: `Nakano Nino\n5 Menit`,
                             thumbnail: fs.readFileSync('./media/menus/Menu1.jpg'),
@@ -34,4 +73,4 @@ handler.tags = ['ᴘᴇɴᴅɪɴɢ sᴛᴜғғ']
 
 handler.command = /^troli$/i
 
-export default handler
+export default handler*/
