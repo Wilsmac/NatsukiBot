38031010efcced64e2cc91028f9a1fa799f1f354
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-13 20:12:13 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -19,7 +19,7 @@ let str = `*ESTOS SON LOS GRUPOS OFICIALES*
 ⏤͟͟͞͞★꙲⃝͟🌹 *${nnnttt2}*\n
 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
 *hola :D*`
-await conn.sendFile(m.chat, imagen, str, fakemek)}
+await conn.sendFile(m.chat, imagen, 'error.jpg', str, fakemek)}
 
 handler.command = /^linkgc|grupos|gruposfantasy|groupofc$/i
 handler.exp = 33
