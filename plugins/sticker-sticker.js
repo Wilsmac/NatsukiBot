ed899f09f83d8c0a83dbc6b9970b71eb27af3a21
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-15 15:12:09 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -39,7 +39,7 @@ if (new Date - user.lastmiming < 10000) return await conn.reply(m.chat, `*ESPERA
     console.error(e)
     if (!stiker) stiker = e
   } finally {
-     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, channelWa })
+     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: gt, mediaType: 2, sourceUrl: nn, thumbnail: imagen1}}}, { quoted: m })
     else throw `> ⓘ _𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚀𝚄𝙴 𝚃𝙴𝚁𝙼𝙸𝙽𝙴 𝙲𝙾𝙽 \`.jpg o .png\` 𝚈 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}_`
   }
 user.lastmiming = new Date * 1
