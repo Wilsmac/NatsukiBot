118cd51571e6316f112e665d47acadd874f9389b
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-14 19:44:58 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

---
+++ plugins/settings.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+import axios from 'axios'
+import fs from 'fs'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto }  = (await import(global.baileys))
+
+var handler = m => m
+handler.all = async function (m) {
+
+
+var canal = [
+{ link: canal1, id: "120363192435027853@newsletter", name: "『 Natsuki Club Oficial 』" }
+]
+var Channelswa = canal
+
+global.channelWa = { contextInfo: { mentionedJid: null, forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: Channelswa.id, serverMessageId: '', newsletterName: Channelswa.name }, externalAdReply: { title: wm, body: gt, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: Menu1, thumbnail: imagen1, sourceUrl: canalofc }}}, { quoted: m }
+
+}
+export default handler
