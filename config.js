2221733d15626ba476731b482bf446feb238bef6
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-15 15:38:07 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- config.js
+++ config.js
@@ -127,7 +127,7 @@ global.año = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
 var canal = [
-{ link: canalofc, id: "120363192435027853@newsletter", name: "『 Natsuki Club Oficial 』" }
+{ link: canalofc, id: "120363192435027853@newsletter", name: "『 𝑈𝑁𝐼𝐹𝐼𝑁𝐸𝐷 𝑂𝐹𝐶 』" }
 ]
 var channelsLPD = canal
 
