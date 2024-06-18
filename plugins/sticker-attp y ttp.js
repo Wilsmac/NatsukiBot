e37bce219bf6092a4bef27bc3c41aa028b04b33a
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-18 02:06:38 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/sticker-attp y ttp.js
+++ plugins/sticker-attp y ttp.js
@@ -1,16 +1,17 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
-let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+let handler = async(m, { conn: natsuki, text, args, usedPrefix, command }) => {
 if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙋𝘼𝙍𝘼 𝙌𝙐𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙎𝙀 𝘾𝙊𝙉𝙑𝙄𝙀𝙍𝙏𝘼 𝙀𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* Hello World`
 let teks = encodeURI(text)
 
 if (command == 'attp') {
 let stiker = await sticker(null,`https://api.erdwpe.com/api/maker/attp?text=${teks}`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+natsuki.sendFile(m.chat, stiker, null, { asSticker: true })}
 
 if (command == 'ttp') {
 let stiker = await sticker(null,`https://api.erdwpe.com/api/maker/ttp?text=${teks}`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+
+natsuki.sendFile(m.chat, stiker, null, { asSticker: true })}
 }
 handler.command = handler.help = ['ttp', 'attp']
 handler.tags = ['sticker']
