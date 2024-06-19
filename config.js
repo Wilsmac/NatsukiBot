e89cc37adec89b6220af3986d502bd82bf410614
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-18 18:57:38 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- config.js
+++ config.js
@@ -165,7 +165,7 @@ global.channel3 = 'https://t.me/'
 //------------------------------------------------
 
 global.Fakechannel = contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ Team Comunity Natsuki 』", serverMessageId: -1 }, forwardingScore: 999, externalAdReply: { title: gt, body: wm, thumbnailUrl: fs.readFileSync('./media/menus/Menu3.jpg'), sourceUrl: md, mediaType: 1, renderLargerThumbnail: false
-}}})
+}}
 
 //------------------------------------------------
 
