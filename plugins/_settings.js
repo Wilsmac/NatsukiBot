0bc1d9744d575cdf85bfa09159ac4d96fc62d2ba
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-18 19:17:49 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_settings.js
+++ plugins/_settings.js
@@ -9,6 +9,11 @@ handler.all = async function (m) {
 
 //------------------------------------------------
 
+global.FakeChannel = { contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ Team Comunity Natsuki 』", serverMessageId: -1 }
+}}, { quoted: m }
+
+//-----------------------------------------------
+
 global.Fakechannel = { contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ Team Comunity Natsuki 』", serverMessageId: -1 }, forwardingScore: 999, externalAdReply: { title: gt, body: wm, thumbnailUrl: fantasyImg.getRandom(), sourceUrl: md, mediaType: 1, renderLargerThumbnail: false
 }}}, { quoted: m }
 
