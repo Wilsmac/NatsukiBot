e351852ba9999cf37da31b789bcab77916f07a21
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-18 19:11:07 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -10,8 +10,8 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 1000) }) * 1000}
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let uptime = clockString(_uptime)
-let estado = `╭━━━━[ *𝙀𝙎𝙏𝘼𝘿𝙊* ]━━━━━⬣
-┃💗 *¡Hola* ${name}
+let estado = `╭━━━━『 𝙴𝚂𝚃𝙰𝙳𝙾 』━━━━━⬣
+┃ *¡Hola* ${name}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ 
 ┃ \`Versión de ${gt}\`
 ┃➥ ${vs}
