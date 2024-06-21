f3a45a2a92c0639f36faf09d801064fc3c547532
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-21 17:06:04 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ import { unwatchFile, watchFile } from 'fs'
 import fs from 'fs'
 import chalk from 'chalk'   
 import fetch from 'node-fetch'
-import './plugins/_content.js'
+import './plugins/_settings.js'
  
 /**
  * @type {import('@adiwajshing/baileys')}  
