cb577181da7c32040e31b187065f70cec8806b7d
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-21 09:59:31 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.botNumberCode = ""
 global.confirmCode = "" 
 //-----------------------------------------------
 
-global.catalogo = fs.readFileSync('./media/menus/estilos.png')
+global.catalogo = fs.readFileSync('./media/menus/catalogo.png')
 
 //------------------------------------------------
 global.lenguajeCD = es
