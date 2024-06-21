aed21570a5f6bf8b0030630b11cf066ba6ff2867
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-20 23:04:58 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/p-prueba2.js
+++ plugins/p-prueba2.js
@@ -1,1 +1,16 @@
-// hola 👋🏻
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'mmmmm'
+  m.reply('Procesando...')
+  let res = `https://bx-hunter.herokuapp.com/api/canvas/gfx2?apikey=FuckBitch&nama=${response[0]}`
+  conn.sendFile(m.chat, res, 'error.jpg', `hhjjj`, m, false)
+}
+handler.help = ['logololi'].map(v => v + ' <text>')
+handler.tags = ['nulis', 'anime']
+handler.command = /^(logololi)$/i
+handler.register = true
+
+handler.limit = true
+
+export default handler
