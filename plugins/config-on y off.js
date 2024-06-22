e5fa529a90003d026d4c55af0b354b1c9ecfdc12
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-06-21 18:31:10 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -653,11 +653,11 @@ throw false
 }
 /*await conn.reply(m.chat, `${lenguajeGB['smsAvisoRG']()}➥ *_${lenguajeGB['smsMens1']()}_* > ${type} 
 ➥ *_${lenguajeGB['smsMens2']()}_* > ${isEnable ? lenguajeGB.smsEncender() : lenguajeGB.smsApagar()} 
-➥ *_${lenguajeGB['smsMens3']()}_* > ${isAll ? lenguajeGB.smsMens4() : isUser ? '' : lenguajeGB.smsMens5()}\n╚═════•| ✿ |•═════╝`, fkontak2, m)}*/
+➥ *_${lenguajeGB['smsMens3']()}_* > ${isAll ? lenguajeGB.smsMens4() : isUser ? '' : lenguajeGB.smsMens5()}\n╚═════•| ✿ |•═════╝`, estilo2, m)}*/
 
 await conn.sendButton(m.chat, `➥ _${lenguajeCD['smsMens1']()}_ ${type} 
 ➥ _${lenguajeCD['smsMens2']()}_  ${isEnable ? lenguajeCD.smsEncender() : lenguajeCD.smsApagar()} 
-➥ _${lenguajeGB['smsMens3']()}_  ${isAll ? lenguajeCD.smsMens4() : isUser ? '' : lenguajeCD.smsMens5()}`, wm, null, [[`${isEnable ? lenguajeGB.smsApagar() : lenguajeCD.smsEncender()}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`]], null, null, fkontak2)}
+➥ _${lenguajeGB['smsMens3']()}_  ${isAll ? lenguajeCD.smsMens4() : isUser ? '' : lenguajeCD.smsMens5()}`, wm, null, [[`${isEnable ? lenguajeGB.smsApagar() : lenguajeCD.smsEncender()}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`]], null, null, estilo2)}
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
