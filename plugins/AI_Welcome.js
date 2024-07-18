5a8516d4115b68ffbb6ff0aed2ba38189249364f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-07-18 11:32:54 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/AI_Welcome.js
+++ plugins/AI_Welcome.js
@@ -1,62 +1,31 @@
-/*import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+let handler = async (m, { conn, usedPrefix: _p }) => {
+  try {
+    let pp = await conn.profilePictureUrl(m.sender, 'image')
 
-export async function before(m, {conn: this, participants, groupMetadata}) {
-  if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/27broato')
-  let img = await (await fetch(`${pp}`)).buffer()
-  let chat = global.db.data.chats[m.chat]
+    const nst = {
+      "key": {
+        "fromMe": false,
+        "participant": "0@s.whatsapp.net",
+        "remoteJid": "0@s.whatsapp.net"
+      },
+      "message": {
+        "groupInviteMessage": {
+          "groupJid": "6285240750713-1610340626@g.us",
+          "inviteCode": "mememteeeekkeke",
+          "groupName": "P",
+          "caption": "Hello, I'm NatsukiBot",
+          "jpegThumbnail": await (await fetch(pp)).buffer()
+        }
+      }
+    }
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `${lenguajeCD['smsWelcome']()}`
-this.sendMessage(m.chat { text: bienvenida, 
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: true, 
-mentionedJid:[user],
-"externalAdReply": {
-"showAdAttribution": true,
-"renderLargerThumbnail": true,
-"thumbnail": img, 
-"title": gt,
-"containsAutoReply": false,
-"mediaType": 1, 
-sourceUrl: canalofc }}}, { quoted: estilo2 })
-//await conn.sendNatsuki2(m.chat, gt, textia, bienvenida, img, img, canalofc, estilo2)
+    conn.sendMessage(m.chat, { text: 'Hola soy Hola, soy Natsuki, en que puedo ayudarte?' }, { quoted: nst })
+  } catch (error) {
+    conn.sendMessage(m.chat, 'Hola, soy Natsuki, en que puedo ayudarte?', 'conversation', { quoted: m })
   }
+}
 
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = `${lenguajeCD['smsBye']()}`
-this.sendMessage(m.chat { text: bye, 
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: true, 
-mentionedJid:[user],
-"externalAdReply": {
-"showAdAttribution": true,
-"renderLargerThumbnail": true,
-"thumbnail": img, 
-"title": gt,
-"containsAutoReply": false,
-"mediaType": 1, 
-sourceUrl: canalofc }}}, { quoted: estilo2 })
-//await conn.sendNatsuki2(m.chat, gt, textia, bye, img, img, canalofc, estilo2)
-  }
-
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = `${lenguajeCD['smsBye']()}`
-this.sendMessage(m.chat { text: kick, 
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: true, 
-mentionedJid:[user],
-"externalAdReply": {
-"showAdAttribution": true,
-"renderLargerThumbnail": true,
-"thumbnail": img, 
-"title": gt,
-"containsAutoReply": false,
-"mediaType": 1, 
-sourceUrl: canalofc }}}, { quoted: estilo2 })
-//await conn.sendNatsuki2(m.chat, gt, textia, kick, img, img, canalofc, estilo2)
-}}*/
+handler.customPrefix = /^(Ai)$/i
+handler.command = new RegExp
+export default handler
