91a77f370f528d8360e4d853ad42b4e70972093e
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-07-18 11:37:09 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/AI_Welcome.js
+++ plugins/AI_Welcome.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       }
     }
 
-    conn.reply(m.chat, { text: 'Hola soy Hola, soy Natsuki, en que puedo ayudarte?' }, { quoted: nst })
+    conn.reply(m.chat, { text: 'Hola soy Hola, soy Natsuki, en que puedo ayudarte?' }, { quoted: m} nst)
   } catch (error) {
     conn.reply(m.chat, 'Hola, soy Natsuki, en que puedo ayudarte?', 'conversation', { quoted: m })
   }
