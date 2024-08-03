5c824b4f5cc681a3529ab3475449904e25b4cd74
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-08-02 19:01:02 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,4 +1,53 @@
-import fg from 'api-dylux';
+ import { igdl } from 'ruhend-scraper';
+
+const handler = async (m, { text, conn, args, usedPrefix, command }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, 'Ingresa Un Link De Facebook', m);
+  }
+
+  let res;
+  try {
+    res = await igdl(args[0]);
+  } catch (error) {
+    return conn.reply(m.chat, 'Error al obtener datos. Verifica el enlace.', m);
+  }
+
+  let result = res.data;
+  if (!result || result.length === 0) {
+    return conn.reply(m.chat, 'No se encontraron resultados.', m);
+  }
+
+  let data;
+  try {
+    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
+  } catch (error) {
+    return conn.reply(m.chat, 'Error al procesar los datos.', m);
+  }
+
+  if (!data) {
+    return conn.reply(m.chat, 'No se encontró una resolución adecuada.', m);
+  }
+
+  let video = data.url;
+  try {
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: null, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
+  } catch (error) {
+    return conn.reply(m.chat, 'Error al enviar el video.', m);
+  }
+};
+
+handler.command = /^(facebook|fb)$/i;
+
+export default handler;    
+
+
+
+
+
+
+
+
+/*import fg from 'api-dylux';
 import fetch from 'node-fetch';
 import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
 import fbDownloader from 'fb-downloader-scrapper';
@@ -90,7 +139,7 @@ reject(e.message);
 
 
 
-/*import fg from 'api-dylux' 
+import fg from 'api-dylux' 
 import fetch from 'node-fetch'
 import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
 import fbDownloader from 'fb-downloader-scrapper'
