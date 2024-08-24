2c511a40c32be5885d50acf53097addac671c585
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-08-24 15:02:32 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/Nsfw-rule34.js
+++ plugins/Nsfw-rule34.js
@@ -1,3 +1,22 @@
+let handler = async (m, { conn, participants, groupMetadata }) => {
+
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.png'
+
+    let text =`_Hola, sean todos bienvenidos a:_\n *${groupMetadata.subject}*
+
+${groupMetadata.desc?.toString() || 'desconocido'}
+`.trim()
+const mentionedJid = groupMetadata.participants.map(v => v.id);
+    conn.sendFile(m.chat, pp, 'pp.jpg', text, m, true, { mentions: mentionedJid})
+}
+
+handler.command = ['welcome','bienvenidos','bienbenidos'] 
+handler.group = true
+handler.admin = true
+
+export default handler
+
+
 /*import Natsukiteam from "@NatsukiTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
