d95f795384e4afbff882432b7f571a87ff284197
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-08-26 20:02:15 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/AI_Welcome.js
+++ plugins/AI_Welcome.js
@@ -1,4 +1,15 @@
-import fetch from 'node-fetch';
+let handler = async (m, { conn, usedPrefix, command}) => {
+await conn.reply(m.chat,`Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal. :"v`, estilo)
+}
+handler.customPrefix = /^(Fototeta|fototeta)$/i
+handler.command = new RegExp
+export default handler
+
+
+
+
+
+/*import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
     let pp = await conn.profilePictureUrl(m.sender, 'image')
@@ -27,4 +38,4 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 }
 handler.customPrefix = /^(Ai)$/i
 handler.command = new RegExp
-export default handler
+export default handler*/
