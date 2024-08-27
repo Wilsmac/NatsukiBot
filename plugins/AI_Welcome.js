9eacfe90ead838d164483dcb823d1e3bb406aacf
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-08-26 19:58:47 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/AI_Welcome.js
+++ plugins/AI_Welcome.js
@@ -20,9 +20,9 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       }
     }
 
-    conn.sendMessage(m.chat, { text: 'Hola, soy Natsuki, en que puedo ayudarte?' }, { quoted: estilo2 })
+    conn.sendMessage(m.chat, { text: 'Hola, soy Natsuki, en que puedo ayudarte?' }, { quoted: nst })
   } catch (error) {
-    conn.sendMessage(m.chat, 'Hola, soy Natsuki, en que puedo ayudarte?', 'conversation', { quoted: estilo2 })
+    conn.sendMessage(m.chat, 'Hola, soy Natsuki, en que puedo ayudarte?', 'conversation', { quoted: m })
   }
 }
 handler.customPrefix = /^(Ai)$/i
