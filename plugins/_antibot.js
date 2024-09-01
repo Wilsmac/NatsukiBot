d66d14c1a6f00efb2924824c66346c09d0fceef0
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-09-01 09:04:30 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_antibot.js
+++ plugins/_antibot.js
@@ -1,24 +1,19 @@
-import { areJidsSameUser } from '@whiskeysockets/baileys'
-
-export async function before(m, { participants, conn }) {
-    if (m.isGroup) {
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    if (!m.isGroup) return;
     let chat = global.db.data.chats[m.chat]
+    let delet = m.key.participant
+    let bang = m.key.id
+    let bot = global.db.data.settings[this.user.jid] || {}
+    if (m.fromMe) return true;
 
-        if (!chat.antiBots) {
-            return
-        }
+    if (m.id.startsWith('3EB0') && m.id.length === 22) {
+        let chat = global.db.data.chats[m.chat];
 
-        let botJid = global.conn.user.jid
+        if (chat.antiBot) {
 
-        if (botJid === conn.user.jid) {
-            return
-        } else {
-            let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
-
-            if (isBotPresent) {
-                setTimeout(async () => {
-                    await this.groupLeave(m.chat)
-                }, 5000)
+            if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
             }
         }
     }
