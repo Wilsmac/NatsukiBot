3459f54ac20074de84793a76467218eac204263f
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-09-20 18:14:06 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/command-invalid.js
+++ plugins/command-invalid.js
@@ -1,4 +1,4 @@
-export async function before(m) {
+/*export async function before(m) {
   if (!m.text || !global.prefix.test(m.text)) {
     return;
   }
@@ -28,4 +28,4 @@ export async function before(m) {
    const comando = m.text.trim().split(' ')[0];
    await m.reply(`El comando "${comando}" no existe.\nUsa */menu* para ver la lista de los comandos.`);
   }
-}
+}*/
