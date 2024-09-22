062598c2ed7879cea403964bfc821c433d7669fe
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-09-21 18:29:46 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,4 +1,24 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+    let isClose = {
+        'abrir': 'not_announcement',
+        'cerrar': 'announcement',
+    }[(args[0] || '')]
+    if (isClose === undefined)
+        return conn.reply(`Elija una opción.\n\n*${usedPrefix + command}* abrir\n${usedPrefix + command}* cerrar`, m, canalofc)
+    await conn.groupSettingUpdate(m.chat, isClose)
+}
+handler.help = ['group *abrir/cerrar*']
+handler.tags = ['group']
+handler.command = ['group', 'grupo'] 
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
+
+
+
+
+/*let handler = async (m, { conn, args, usedPrefix, command }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg'  
 let isClose = { // Switch Case Like :v
 'open': 'not_announcement',
@@ -29,4 +49,4 @@ handler.command = /^(group|grupo)$/i
 handler.admin = true
 handler.botAdmin = true
 handler.exp = 200
-export default handler
+export default handler*/
