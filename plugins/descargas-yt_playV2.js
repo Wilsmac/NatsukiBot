663d0a14eec23630b43c63c782ca11d52add3aba
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-10-06 18:16:10 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/descargas-yt_playV2.js
+++ plugins/descargas-yt_playV2.js
@@ -1,3 +1,111 @@
+import fetch from 'node-fetch';
+import yts from 'yt-search';
+import ytdl from 'ytdl-core';
+import axios from 'axios';
+import fg from 'api-dylux';
+
+const handler = async (m, { command, usedPrefix, conn, text }) => {
+    if (!text) throw `${mg}${mid.smsMalused4}\n*${usedPrefix + command} Billie Eilish - Bellyache*`;
+
+    try {
+        if (command === 'play.1') {
+            conn.reply(m.chat, lenguajeCD['smsAvisoEG']() + mid.smsAud, m, {
+                contextInfo: {
+                    externalAdReply: {
+                        mediaUrl: null,
+                        mediaType: 1,
+                        description: null,
+                        title: wm,
+                        body: '✰ 𝚃𝙴𝙰𝙼 𝙽𝚂𝙺 ✰',
+                        previewType: 0,
+                        thumbnail: imagen1,
+                        sourceUrl: canalofc
+                    }
+                }
+            });
+
+            try {
+               
+                const res = await fetch(`https://skizo.tech/api/y2mate?apikey=GataDios&url=${encodeURIComponent(text)}`);
+                const json = await res.json();
+
+                
+                if (json.convert) {
+                    
+                    await conn.sendMessage(m.chat, {
+                        audio: { url: json.convert },
+                        fileName: `audio.mp3`,
+                        mimetype: 'audio/mp4'
+                    }, { quoted: m });
+                } else {
+                    throw new Error('No se pudo obtener el enlace de conversión.');
+                }
+            } catch (e) {
+                console.error('Error al obtener el audio:', e);
+                await conn.reply(m.chat, `${lenguajeCD['smsMalError3']()}#report ${lenguajeCD['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, m);
+            }
+        }
+
+        if (command === 'play.2') {
+            conn.reply(m.chat, lenguajeCD['smsAvisoEG']() + mid.smsVid, m, {
+                contextInfo: {
+                    externalAdReply: {
+                        mediaUrl: null,
+                        mediaType: 1,
+                        description: null,
+                        title: wm,
+                        body: '✰ 𝚃𝙴𝙰𝙼 𝙽𝚂𝙺 ✰',
+                        previewType: 0,
+                        thumbnail: imagen1,
+                        sourceUrl: canalofc
+                    }
+                }
+            });
+
+            try {
+                const mediaa = await ytPlayVid(text);
+                const aa_2 = await conn.sendMessage(m.chat, {
+                    video: { url: mediaa.result },
+                    fileName: `error.mp4`,
+                    caption: `${wm}`,
+                    thumbnail: mediaa.thumb,
+                    mimetype: 'video/mp4'
+                }, { quoted: m });
+
+                if (!aa_2) {
+                    throw new Error();
+                }
+            } catch {
+                try {
+                    let res0 = await yts(text);
+                    res0 = res0.videos[0];
+                    let yt0 = await fg.ytv(res0.url, '360p');
+                    await conn.sendFile(m.chat, yt0.dl_url, 'error.mp4', `${wm}`, m);
+                } catch {
+                    const res = await fetch(`https://skizo.tech/api/y2mate?apikey=GataDios&url=${encodeURIComponent(text)}`);
+                    const json = await res.json();
+                    await conn.sendFile(m.chat, json.result.video, 'error.mp4', `${wm}`, m);
+                }
+            }
+        }
+    } catch (e) {
+        await conn.reply(m.chat, `${lenguajeCD['smsMalError3']()}#report ${lenguajeCD['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, m);
+        console.log(`❗❗ ${lenguajeCD['smsMensError2']()} ${usedPrefix + command} ❗❗`);
+        console.log(e);
+        handler.limit = 0; // No gastar límite si fallas
+    }
+};
+
+handler.help = ['play.1', 'play.2'].map(v => v + ' <texto>');
+handler.tags = ['downloader'];
+handler.command = ['play.1', 'play.2'];
+handler.limit = 1;
+
+export default handler;
+
+
+
+
 /*import fetch from 'node-fetch';
 import yts from 'yt-search';
 import ytdl from 'ytdl-core';
