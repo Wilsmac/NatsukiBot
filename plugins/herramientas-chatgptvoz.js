5d6cde9008916ff662ec6c853675272cd09467ba
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-10-06 19:12:44 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/herramientas-chatgptvoz.js
+++ plugins/herramientas-chatgptvoz.js
@@ -14,8 +14,7 @@ const idioma = 'es'
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
 
-  const sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Wilsmac, tu seras Natsuki`,
-          ;
+  const sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Wilsmac, tu seras Natsuki`;
 
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   if (!text) throw `𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽𝙰 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽 𝙾 𝚄𝙽𝙰 𝙾𝚁𝙳𝙴𝙽 𝙿𝙰𝚁𝙰 𝚄𝚂𝙰𝚁 𝙻𝙰 𝙵𝚄𝙽𝙲𝙸𝙾𝙽 𝙳𝙴 𝙲𝙷𝙰𝚃𝙶𝙿𝚃*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾𝚂 𝙳𝙴 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽𝙴𝚂 𝚈 𝙾𝚁𝙳𝙴𝙽𝙴𝚂*\n*${usedPrefix + command} Codigo en JS para un juego de cartas`;
