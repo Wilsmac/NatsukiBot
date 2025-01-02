1f39dc4ecec6bb5e590db29ba3c246925b67f9b0
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2025-01-02 12:39:25 -0600

    𝑻𝒆𝒂𝒎 𝑵𝒔𝒌

--- plugins/amensaje-en-espera.js
+++ plugins/amensaje-en-espera.js
@@ -1,4 +1,69 @@
-import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
+import { readdirSync, unlinkSync, existsSync, promises as fs } from 'fs';
+import path from 'path';
+
+const handler = async (m, { conn }) => {
+  const sessionPath = './NatsukiSessions';
+  const fileLimit = 10;
+
+  if (global.conn.user.jid !== conn.user.jid) {
+    return conn.sendMessage(m.chat, { text: '⚠️ Este comando solo puede ser utilizado en el bot principal' }, { quoted: m });
+  }
+  await conn.sendMessage(m.chat, { text: '✅ Iniciando...' }, { quoted: m });
+  try {
+    if (!existsSync(sessionPath)) {
+      return await conn.sendMessage(m.chat, { text: '⚠️ La carpeta de sesión de 孫ՏᴏɴᏀᴏᴋᴜ孫 no existe o está vacía.' }, { quoted: m });
+    }
+    const files = await fs.readdir(sessionPath);
+    const totalFiles = files.length;
+    if (totalFiles > fileLimit) {
+      let filesDeleted = 0;
+      for (const file of files) {
+        if (file !== 'creds.json') { 
+          await fs.unlink(path.join(sessionPath, file));
+          filesDeleted++;
+          if (totalFiles - filesDeleted <= fileLimit) break; 
+        }
+      }
+      await conn.sendMessage(m.chat, {
+        text: `⚠️ Se eliminaron ${filesDeleted} archivos basura automáticamente. Total de archivos ahora: ${totalFiles - filesDeleted}.`
+      }, { quoted: m });
+    } else {
+      let filesDeleted = 0;
+      for (const file of files) {
+        if (file !== 'creds.json') {
+          await fs.unlink(path.join(sessionPath, file));
+          filesDeleted++;
+        }
+      }
+      if (filesDeleted === 0) {
+        await conn.sendMessage(m.chat, { text: '⚠️ No se encontró ningún archivo para eliminar en la carpeta de sessions.' }, { quoted: m });
+      } else {
+        await conn.sendMessage(m.chat, { text: `✅ Se eliminaron ${filesDeleted} archivos de ${wm} manualmente.` }, { quoted: m });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    await conn.sendMessage(m.chat, { text: `⚠️ Ocurrió un error al eliminar los archivos de sesión` }, { quoted: m });
+  }
+  await conn.sendMessage(m.chat, { text: `¡Hola! ¿Ahora me ves? Att: ՏᴏɴᏀᴏᴋᴜ.` }, { quoted: m });
+};
+
+handler.tags = ['owner'];
+handler.help = ['dsowner', 'ds'];
+handler.command = /^(del_reg_in_session_owner|dsowner|clearallsession)$/i;
+handler.rowner = true;
+
+export default handler;
+
+
+
+
+
+
+
+
+
+/*import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
 import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
@@ -46,8 +111,7 @@ handler.command = /^(fixmsgespera|ds)$/i;
 
 
 
-
-/*import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
+import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
 import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
