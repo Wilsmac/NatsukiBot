56a95624d25368750350b95e206a19e1e04a6212
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2025-01-17 19:57:45 -0600

    𝑻𝒆𝒂𝒎 𝑵𝒔𝒌

--- plugins/propietario(a)-reiniciar-usuario.js
+++ plugins/propietario(a)-reiniciar-usuario.js
@@ -9,7 +9,7 @@ var number = text.split`@`[1]
 var number = text
 }
 
-if(!text && !m.quoted) return conn.reply(m.chat, `*ETIQUETE AL USUARIO, ESCRIBA SU NUMERO O RESPONDA AL MENSAJE PARA REINICIAR DATOS*`, m)
+if(!text && !m.quoted) return conn.reply(m.chat, `*ETIQUETE AL USUARIO, ESCRIBA SU NUMERO O RESPONDA AL MENSAJE PARA REINICIAR SUS DATOS*`, m)
 if(isNaN(number)) return conn.reply(m.chat, `*EL NÚMERO QIE INGRESÓ NO ES VÁLIDO PARA REINICIAR LOS DATOS*`, m)
 try {
 if(text) {
